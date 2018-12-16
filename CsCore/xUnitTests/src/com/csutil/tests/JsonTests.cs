4d39036be4f3a2185c1acad9b1c66a893565e09f
S <code@csutil.com>
2018-12-16 21:48:48 +0100

    Added test to check that log warnings are printed for missing fields

--- CsCore/xUnitTests/src/com/csutil/tests/JsonTests.cs
+++ CsCore/xUnitTests/src/com/csutil/tests/JsonTests.cs
@@ -12,8 +12,16 @@ namespace com.csutil.tests {
         }
 
         [Fact]
-        public void Test1() {
-            MySubClass1 x1 = new MySubClass1("test");
+        public void TestClassWithoutDefaultConstructor() {
+            var x1 = new MySubClass1("test");
+            var json = JsonWriter.NewWriter().Write((MyClass1)x1);
+            var x2 = JsonReader.NewReader().Read<MySubClass1>(json);
+            Assert.Equal(x1.s, x2.s);
+        }
+
+        [Fact]
+        public void TestMissingFieldInClass() {
+            var x1 = new MySubClass2() { s = "s", s2 = "s2" };
             var json = JsonWriter.NewWriter().Write((MyClass1)x1);
             var x2 = JsonReader.NewReader().Read<MySubClass1>(json);
             Assert.Equal(x1.s, x2.s);
@@ -24,6 +32,11 @@ namespace com.csutil.tests {
             public string s;
             public MySubClass1(string s) : base() { this.s = s; }
         }
+        private class MySubClass2 : MyClass1 {
+            public string s;
+            public string s2;
+        }
+
 
     }
 }
