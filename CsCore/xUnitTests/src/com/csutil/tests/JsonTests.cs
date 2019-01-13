078fc9202187e7144474eb17ddd3b47cd31f669b
S <code@csutil.com>
2019-01-13 08:14:51 +0100

    some name changes

--- CsCore/xUnitTests/src/com/csutil/tests/JsonTests.cs
+++ CsCore/xUnitTests/src/com/csutil/tests/JsonTests.cs
@@ -11,43 +11,48 @@ namespace com.csutil.tests {
 
         [Fact]
         public void TestClassWithoutDefaultConstructor() {
-            var x1 = new MySubClass1("I am myString");
+            var x1 = new MySubClassWithoutADefaultConstructor("I am myString");
             var json = JsonWriter.GetWriter().Write((MyClass1)x1);
-            var x2 = JsonReader.GetReader().Read<MySubClass1>(json);
+            var x2 = JsonReader.GetReader().Read<MySubClassWithoutADefaultConstructor>(json);
             Assert.Equal(x1.myString, x2.myString);
         }
 
         [Fact]
         public void TestMissingFieldInClass() {
-            var x1 = new MySubClass2() { myString = "I am s1", myString2 = "I am s2" };
+            var x1 = new MySubClass1() { myString = "I am s1", myString2 = "I am s2" };
             var json = JsonWriter.GetWriter().Write((MyClass1)x1);
-            var x2 = JsonReader.GetReader().Read<MySubClass1>(json);
+            var x2 = JsonReader.GetReader().Read<MySubClassWithoutADefaultConstructor>(json);
             Assert.Equal(x1.myString, x2.myString);
         }
 
         [Fact]
         public void TestMissingFieldInClass2() {
-            var x1 = new MySubClass2() { myString = "I am s1", myString2 = "I am s2" };
+            var x1 = new MySubClass1() { myString = "I am s1", myString2 = "I am s2" };
             var json = JsonWriter.GetWriter().Write(x1);
             Log.d("json=" + json);
-            var x3 = JsonReader.GetReader().Read<MySubClass3>(json);
+            var x3 = JsonReader.GetReader().Read<MySubClassThatKeepsAdditionalJsonFields>(json);
             Assert.Equal(x1.myString, x3.myString);
         }
 
-        private class MyClass1 { }
+        private class MyClass1 {
+
+        }
+
         private class MySubClass1 : MyClass1 {
             public string myString;
+            public string myString2;
+
+            public Int64 myInt { get; set; }
+        }
+
+        private class MySubClassWithoutADefaultConstructor : MyClass1 {
+            public string myString { get; set; }
 
             //The class has no default constructor but still can be instantiated by the JSON logic
-            public MySubClass1(string s) : base() { myString = s; }
+            public MySubClassWithoutADefaultConstructor(string s) : base() { myString = s; }
         }
 
-        private class MySubClass2 : MyClass1 {
-            public string myString;
-            public string myString2;
-        }
-
-        private class MySubClass3 : MyClass1, HandleAdditionalJsonFields {
+        private class MySubClassThatKeepsAdditionalJsonFields : MyClass1, HandleAdditionalJsonFields {
             public string myString;
 
             private Dictionary<string, object> missingFields;
