e34dce4cceb586732d4e97b7dc166356a392451d
S <code@csutil.com>
2019-02-04 09:12:10 +0100

    Added some more logging while trying to find a way to run the dll logic on asset package import

--- CsCore/CsCoreNet3.5Compat/Plugins/CsCoreNet3.5Compat/editor/SystemThreadingRenamePostprocessor.cs
+++ CsCore/CsCoreNet3.5Compat/Plugins/CsCoreNet3.5Compat/editor/SystemThreadingRenamePostprocessor.cs
@@ -9,15 +9,35 @@ class SystemThreadingRenamePostprocessor : AssetPostprocessor {
     private const string STDLL = "System.Threading.dll";
     private const string STDLL_BACKUP = STDLL + ".backup";
 
+    [RuntimeInitializeOnLoadMethod]
+    static void RuntimeInitializeOnLoadMethod() {
+        Debug.LogError("RuntimeInitializeOnLoadMethod");
+    }
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    static void DidReloadScripts() {
+        Debug.LogError("UnityEditor.Callbacks.DidReloadScripts");
+    }
+
+    [UnityEditor.Callbacks.OnOpenAsset]
+    static void OnOpenAsset() {
+        Debug.LogError("UnityEditor.Callbacks.OnOpenAsset");
+    }
+
+    [UnityEditor.Callbacks.PostProcessBuild]
+    static void PostProcessBuild() {
+        Debug.LogError("UnityEditor.Callbacks.PostProcessBuild");
+    }
+
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
         foreach (string str in importedAssets) {
             if (str.EndsWith(STDLL) || str.EndsWith(STDLL_BACKUP)) {
-                renameThreadingDllIfNet4_6(new FileInfo(str));
+                RenameThreadingDllBasedOnDotNetVersion(new FileInfo(str));
             }
         }
     }
 
-    private static void renameThreadingDllIfNet4_6(FileInfo threadingDll) {
+    private static void RenameThreadingDllBasedOnDotNetVersion(FileInfo threadingDll) {
         if (!threadingDll.Exists) { return; }
 #if NET_4_6
         Debug.LogError(STDLL + " not needed in .net 4.6+, will rename it. " + threadingDll);
