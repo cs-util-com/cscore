f078d821276f0fe86a68b97cae5f0fd86194477c
S <code@csutil.com>
2019-02-05 09:34:32 +0100

    Cleanup of dll renamer script, currently this script does not run on initial import of the asset and I did not find a way yet to execute such cleanup code

--- CsCore/CsCoreNet3.5Compat/Plugins/CsCoreNet3.5Compat/editor/SystemThreadingRenamePostprocessor.cs
+++ CsCore/CsCoreNet3.5Compat/Plugins/CsCoreNet3.5Compat/editor/SystemThreadingRenamePostprocessor.cs
@@ -9,26 +9,6 @@ class SystemThreadingRenamePostprocessor : AssetPostprocessor {
     private const string STDLL = "System.Threading.dll";
     private const string STDLL_BACKUP = STDLL + ".backup";
 
-    [RuntimeInitializeOnLoadMethod]
-    static void RuntimeInitializeOnLoadMethod() {
-        Debug.LogError("RuntimeInitializeOnLoadMethod");
-    }
-
-    [UnityEditor.Callbacks.DidReloadScripts]
-    static void DidReloadScripts() {
-        Debug.LogError("UnityEditor.Callbacks.DidReloadScripts");
-    }
-
-    [UnityEditor.Callbacks.OnOpenAsset]
-    static void OnOpenAsset() {
-        Debug.LogError("UnityEditor.Callbacks.OnOpenAsset");
-    }
-
-    [UnityEditor.Callbacks.PostProcessBuild]
-    static void PostProcessBuild() {
-        Debug.LogError("UnityEditor.Callbacks.PostProcessBuild");
-    }
-
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
         foreach (string str in importedAssets) {
             if (str.EndsWith(STDLL) || str.EndsWith(STDLL_BACKUP)) {
@@ -40,18 +20,35 @@ class SystemThreadingRenamePostprocessor : AssetPostprocessor {
     private static void RenameThreadingDllBasedOnDotNetVersion(FileInfo threadingDll) {
         if (!threadingDll.Exists) { return; }
 #if NET_4_6
-        Debug.LogError(STDLL + " not needed in .net 4.6+, will rename it. " + threadingDll);
-        RenameFile(threadingDll, STDLL_BACKUP);
+        if (RenameFile(threadingDll, STDLL_BACKUP)) {
+            Debug.LogError(STDLL + " not needed in .net 4.6+, deactivated dll " + threadingDll.FullName);
+        }
 #elif NET_2_0 || NET_2_0_SUBSET
-        Debug.LogError(STDLL + " needed previous to .net 4.6+, will rename it. " + threadingDll);
-        RenameFile(threadingDll, STDLL);
+        if (RenameFile(threadingDll, STDLL)) {
+            Debug.LogError(STDLL + " needed previous to .net 4.6+, restored dll from " + threadingDll.FullName);
+        }
 #endif
     }
 
-    private static void RenameFile(FileInfo self, string newFileName) {
+    private static bool RenameFile(FileInfo self, string newFileName) {
         var oldPath = self.FullName;
         var newPath = self.Directory.FullName + Path.DirectorySeparatorChar + newFileName;
-        if (oldPath != newPath) { File.Move(oldPath, newPath); }
+        if (oldPath != newPath) { File.Move(oldPath, newPath); return true; }
+        return false;
     }
+
+    // Some more logging:
+
+    [RuntimeInitializeOnLoadMethod]
+    static void RuntimeInitializeOnLoadMethod() { Debug.Log("RuntimeInitializeOnLoadMethod"); }
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    static void DidReloadScripts() { Debug.LogError("UnityEditor.Callbacks.DidReloadScripts"); }
+
+    [UnityEditor.Callbacks.OnOpenAsset]
+    static void OnOpenAsset() { Debug.Log("UnityEditor.Callbacks.OnOpenAsset"); }
+
+    [UnityEditor.Callbacks.PostProcessBuild]
+    static void PostProcessBuild() { Debug.Log("UnityEditor.Callbacks.PostProcessBuild"); }
 
 }
