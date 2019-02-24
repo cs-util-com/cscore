2de9ff54a55622cbe271a02f4f538daff666c7a3
S <code@csutil.com>
2019-02-24 11:51:44 +0100

    Added quick links

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/editor/CsUtilUnityEditorMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsUtilUnityEditorMenu {
+
+    [UnityEditor.MenuItem("CsUtil/Open MaterialUi Icon Overview")]
+    static void OpenMaterialUiIconOverview() { Application.OpenURL("https://shanfan.github.io/material-icons-cheatsheet/"); }
+
+    [UnityEditor.MenuItem("CsUtil/CsCore/Open GitHub page")]
+    static void CsCoreGithubPage() { Application.OpenURL("https://github.com/cs-util-com/cscore"); }
+
+    [UnityEditor.MenuItem("CsUtil/CsCore/Report a problem")]
+    static void ReportCsCoreProblem() { Application.OpenURL("https://github.com/cs-util-com/cscore/issues"); }
+
+    [UnityEditor.MenuItem("CsUtil/Show all Asset Store packages")]
+    static void ShowAssetStore() { Application.OpenURL("https://assetstore.unity.com/publishers/40989"); }
+
+}
