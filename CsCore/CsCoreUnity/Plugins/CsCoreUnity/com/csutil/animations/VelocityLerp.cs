274ebcbfe024333074fd2c510e441a90b9cc1455
S <code@csutil.com>
2019-03-03 11:35:23 +0100

    Added lerping helpers

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/animations/VelocityLerp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace com.csutil {
+
+    public static class VelocityLerp {
+
+        /// <summary> Critically Damped Spring Smoothing - http://mathproofs.blogspot.jp/2013/07/critically-damped-spring-smoothing.html </summary>
+        /// <param name="currentVelocity"> Should be a field which is passed with every call again </param>
+        /// <returns>the new value</returns>
+        public static float LerpWithVelocity(float self, float destination, ref float currentVelocity, float dt, float omega = 1) {
+            // impl. from https://github.com/keijiro/SmoothingTest/blob/master/Assets/Flask/Tween.cs
+            var n1 = currentVelocity - omega * omega * dt * (self - destination);
+            var n2 = 1 + omega * dt;
+            currentVelocity = n1 / (n2 * n2);
+            return self + currentVelocity * dt;
+        }
+
+        /// <summary> Critically Damped Spring Smoothing - http://mathproofs.blogspot.jp/2013/07/critically-damped-spring-smoothing.html </summary>
+        /// <param name="currentVelocity"> Should be a field which is passed with every call again </param>
+        /// <param name="dt"> Normally Time.deltaTime </param>
+        /// <param name="omega"> Try something between 10 and 100 </param>
+        public static Vector3 LerpWithVelocity(this Vector3 self, Vector3 destination, Vector3 currentVelocity, float dt, float omega = 20f) {
+            var n1 = currentVelocity - (self - destination) * (omega * omega * dt);
+            var n2 = 1 + omega * dt;
+            currentVelocity = n1 / (n2 * n2);
+            return self + currentVelocity * dt;
+        }
+
+        /// <summary> Critically Damped Spring Smoothing - http://mathproofs.blogspot.jp/2013/07/critically-damped-spring-smoothing.html </summary>
+        /// <param name="currentVelocity"> Should be a field which is passed with every call again </param>
+        /// <returns> The same quat. for method chaining </returns>
+        public static Quaternion LerpWithVelocity(this Quaternion self, Quaternion destination, ref Vector4 currentVelocity, float dt, float omega = 1) {
+            // See also https://github.com/keijiro/Klak
+            Vector4 vcurrent = self.ToVector4();
+            Vector4 vtarget = destination.ToVector4();
+            // We can use either of vtarget/-vtarget. Use closer one:
+            if (Vector4.Dot(vcurrent, vtarget) < 0) { vtarget = -vtarget; }
+            var n1 = currentVelocity - (vcurrent - vtarget) * (omega * omega * dt);
+            var n2 = 1 + omega * dt;
+            currentVelocity = n1 / (n2 * n2);
+            var newRot = (vcurrent + currentVelocity * dt);
+            return newRot.ToNormalizedQuaternion(self);
+        }
+
+        public static Quaternion ToNormalizedQuaternion(this Vector4 source, Quaternion target) {
+            source = Vector4.Normalize(source);
+            target.x = source.x;
+            target.y = source.y;
+            target.z = source.z;
+            target.w = source.w;
+            return target;
+        }
+
+        public static Quaternion ToNormalizedQuaternion(this Vector4 self) {
+            self = Vector4.Normalize(self);
+            return new Quaternion(self.x, self.y, self.z, self.w);
+        }
+
+        public static Vector4 ToVector4(this Quaternion self) { return new Vector4(self.x, self.y, self.z, self.w); }
+
+    }
+
+}
+
---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/math/CalculateAverage.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class CalculateAverage {
+
+    /// <summary>
+    /// This allows to calculate the mean of a list without storing all entries of the list.
+    /// See http://math.stackexchange.com/questions/106700/incremental-averageing
+    /// </summary>
+    /// <param name="oldAverage">the old mean value</param>
+    /// <param name="newValue">the new value added to the mean</param>
+    /// <param name="count">the total count of entries considered for the mean</param>
+    /// <returns>the new average</returns>
+    public static float CalcRunningMean(float oldAverage, float newValue, int count) {
+        return (oldAverage * count + newValue) / (count + 1f);
+    }
+
+    /// <summary>
+    /// Fourth order zero-phase shift low-pass Butterworth filter function by Sam Van Wassenbergh (University of Antwerp, 2007)
+    /// Source: http://www.codeproject.com/Tips/1092012/A-Butterworth-Filter-in-Csharp
+    /// </summary>
+    /// <param name="self">The unfiltered data</param>
+    /// <param name="dtInSec">the time between one data point and the next in seconds, is needed to calculate the sampling rate (inverse of the time step)</param>
+    /// <param name="CutOff">desired cutoff frequency in Hz</param>
+    /// <returns></returns>
+    public static double[] CalcButterworthAvg(this double[] self, double dtInSec, double CutOff) {
+        if (self == null) { return null; }
+        if (CutOff == 0) { return self; }
+
+        double Samplingrate = 1 / dtInSec;
+        long dF2 = self.Length - 1;        // The data range is set with dF2
+        double[] Dat2 = new double[dF2 + 4]; // Array with 4 extra points front and back
+        double[] data = self; // Ptr., changes passed data
+
+        // Copy indata to Dat2
+        for (long r = 0; r < dF2; r++) { Dat2[2 + r] = self[r]; }
+        Dat2[1] = Dat2[0] = self[0];
+        Dat2[dF2 + 3] = Dat2[dF2 + 2] = self[dF2];
+        double wc = Math.Tan(CutOff * Math.PI / Samplingrate);
+        double k1 = 1.414213562 * wc; // Sqrt(2) * wc
+        double k2 = wc * wc;
+        double a = k2 / (1 + k1 + k2);
+        double b = 2 * a;
+        double c = a;
+        double k3 = b / k2;
+        double d = -2 * a + k3;
+        double e = 1 - (2 * a) - k3;
+
+        // RECURSIVE TRIGGERS - ENABLE filter is performed (first, last points constant)
+        double[] DatYt = new double[dF2 + 4];
+        DatYt[1] = DatYt[0] = self[0];
+        for (long s = 2; s < dF2 + 2; s++) {
+            DatYt[s] = a * Dat2[s] + b * Dat2[s - 1] + c * Dat2[s - 2] + d * DatYt[s - 1] + e * DatYt[s - 2];
+        }
+        DatYt[dF2 + 3] = DatYt[dF2 + 2] = DatYt[dF2 + 1];
+
+        // FORWARD filter
+        double[] DatZt = new double[dF2 + 2];
+        DatZt[dF2] = DatYt[dF2 + 2];
+        DatZt[dF2 + 1] = DatYt[dF2 + 3];
+        for (long t = -dF2 + 1; t <= 0; t++) {
+            DatZt[-t] = a * DatYt[-t + 2] + b * DatYt[-t + 3] + c * DatYt[-t + 4] + d * DatZt[-t + 1] + e * DatZt[-t + 2];
+        }
+
+        // Calculated points copied for return
+        for (long p = 0; p < dF2; p++) { data[p] = DatZt[p]; }
+        return data;
+    }
+
+}
