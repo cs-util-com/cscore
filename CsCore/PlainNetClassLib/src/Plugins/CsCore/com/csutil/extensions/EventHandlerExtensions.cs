ca4a84872dcfb261c3798a1317d57b9d3cd5f4f7
S <code@csutil.com>
2019-05-28 09:13:33 +0200

    Added a delayed / throttled / debounced execution wrapper for EventHandlers

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/extensions/EventHandlerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.csutil {
+
+    public static class EventHandlerExtensions {
+
+        private static readonly object threadLock = new object();
+
+        /// <summary> 
+        /// This will create an action where the first call is executed and the last call is executed but 
+        /// every call in between that is below the passed millisecond threshold is ignored
+        /// </summary>
+        public static EventHandler<T> AsThrottledDebounce<T>(this EventHandler<T> self, double delayInMs) {
+            bool currentlyThrottling = false;
+            bool needsFinalCall = false;
+            return (sender, eventArgs) => {
+                lock (threadLock) {
+                    if (currentlyThrottling) { needsFinalCall = true; return; }
+                    currentlyThrottling = true;
+                    self(sender, eventArgs);
+                }
+                Task.Delay(TimeSpan.FromMilliseconds(delayInMs)).ContinueWith(_ => {
+                    lock (threadLock) {
+                        if (needsFinalCall) { self(sender, eventArgs); }
+                        currentlyThrottling = false;
+                        needsFinalCall = false;
+                    }
+                });
+            };
+        }
+
+    }
+
+}
