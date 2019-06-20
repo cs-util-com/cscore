cb3232ae769ae43818970c165e24612f585599bb
S <code@csutil.com>
2019-06-20 13:15:10 +0200

    Improved the LiteDb tests

--- CsCore/xUnitTests/src/com/csutil/tests/LiteDbTests.cs
+++ CsCore/xUnitTests/src/com/csutil/tests/LiteDbTests.cs
@@ -1,53 +1,61 @@
+using System;
 using System.Linq;
+using com.csutil.io.db;
 using LiteDB;
 using Xunit;
 
-namespace com.csutil.tests {
+namespace com.csutil.tests.io.db {
 
     public class LiteDbTests {
 
-        public class Customer {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public string[] Phones { get; set; }
-            public bool IsActive { get; set; }
+        public class Customer : HasId {
+            public string id { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+            public string GetId() { return id; }
         }
 
         [Fact]
         void ExampleUsage1() {
+
+            var dbFile = EnvironmentV2.instance.GetAppDataFolder().GetChildDir("tests.io.db").GetChild("ExampleUsage1.db");
+            dbFile.ParentDir().CreateV2();
+            dbFile.DeleteV2(); // for the test ensure that the db file does not yet exist
+
             // Open database (or create if doesn't exist)
-            using (var db = new LiteDatabase(@"MyData.db")) {
+            using (var db = new LiteDatabase(dbFile.FullPath())) {
                 // Get customer collection
                 var customers = db.GetCollection<Customer>("customers");
 
+                string testId = Guid.NewGuid().ToString();
+
                 // Create your new customer instance
                 var customer = new Customer {
-                    Name = "John Doe",
-                    Phones = new string[] { "8000-0000", "9000-0000" },
-                    Age = 39,
-                    IsActive = true
+                    id = testId,
+                    name = "John Doe",
+                    age = 39
                 };
 
                 // Create unique index in Name field
-                customers.EnsureIndex(x => x.Name, true);
+                // https://github.com/mbdavid/LiteDB/wiki/Indexes#ensureindex
+                customers.EnsureIndex(x => x.name, true);
 
-                // Insert new customer document (Id will be auto-incremented)
-                if (customers.Exists(x => x.Name == customer.Name)) {
-                    customers.Update(customer);
-                } else {
-                    customers.Insert(customer);
-                }
+                customers.Insert(customer);
 
-                // Update a document inside a collection
-                customer.Name = "Joana Doe";
+                customer.name = "Joana Doe";
                 customers.Update(customer);
+                Assert.Equal("Joana Doe", customers.FindById(testId).name);
+
+                customer.name = "Joana Doe 2";
+                customers.Upsert(customer); // insert or update if already found
+                Assert.Equal("Joana Doe 2", customers.FindById(testId).name);
 
                 // Use LINQ to query documents (with no index)
-                var results = customers.Find(x => x.Age > 20);
+                var results = customers.Find(x => x.age > 20);
                 Assert.Single(results);
-                Assert.Equal("Joana Doe", results.First().Name);
+                Assert.Equal("Joana Doe 2", results.First().name);
             }
+            Assert.True(dbFile.IsNotNullAndExists());
         }
 
     }
