2d86ebedc78bee882cecd10571d8e28c62510b72
S <code@csutil.com>
2019-07-12 10:05:03 +0200

    Enable memory tracking only in debug mode

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/logging/StopwatchV2.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/logging/StopwatchV2.cs
@@ -12,17 +12,27 @@ namespace com.csutil {
         public long allocatedMemBetweenStartAndStop { get { return memoryAtStop - memoryAtStart; } }
 
         public StopwatchV2 StartV2() {
+            CaptureMemoryAtStart();
+            Start();
+            return this;
+        }
+
+        [Conditional("DEBUG"), Conditional("ENFORCE_FULL_LOGGING")]
+        private void CaptureMemoryAtStart() {
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
             managedMemoryAtStart = GC.GetTotalMemory(true);
             using (var p = Process.GetCurrentProcess()) { memoryAtStart = p.PrivateMemorySize64; }
-            Start();
-            return this;
         }
 
         public void StopV2() {
             Stop();
+            CaptureMemoryAtStop();
+        }
+
+        [Conditional("DEBUG"), Conditional("ENFORCE_FULL_LOGGING")]
+        private void CaptureMemoryAtStop() {
             managedMemoryAtStop = GC.GetTotalMemory(true);
             using (var p = Process.GetCurrentProcess()) { memoryAtStop = p.PrivateMemorySize64; }
         }
