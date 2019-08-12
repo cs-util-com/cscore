42242be072213694ceefb7fc6a3bc6b0b75b41b9
S <code@csutil.com>
2019-08-12 08:18:19 +0200

    Updated TemplateSolution so that it can be quickly copy pasted and still be correctly linked to cscore

--- TemplateSolution/PlainNetClassLib/src/Plugins/AaaaaRenameMeAaaaa/com/csutil/MyClass1.cs
+++ TemplateSolution/PlainNetClassLib/src/Plugins/AaaaaRenameMeAaaaa/com/csutil/MyClass1.cs
@@ -1,17 +1,17 @@
 using System;
 
-namespace com.csutil
-{
-    public class MyClass1
-    {
-        public bool isTrue()
-        {
+namespace com.csutil {
+    public class MyClass1 {
+        public string name;
+        public MyClass1(string name) { this.name = name; }
+
+        public bool isTrue() {
             object x = "'I am x'";
-            if (x is String s)
-            {
+            if (x is String s) {
                 Console.WriteLine("X is a string! s=" + s);
                 return true;
             }
+            Log.d("Test");
             return false;
         }
     }
--- TemplateSolution/xUnitTests/src/com/csutil/tests/UnitTest1.cs
+++ TemplateSolution/xUnitTests/src/com/csutil/tests/UnitTest1.cs
@@ -1,16 +1,21 @@
 using System;
 using Xunit;
 
-namespace com.csutil.tests
-{
-    public class UnitTest1
-    {
+namespace com.csutil.tests {
+
+    public class UnitTest1 {
+
         [Fact]
-        public void Test1()
-        {
-            var x = new MyClass1();
+        public void Test1() {
+            // Test using classes from the main lib:
+            var x = new MyClass1("Abc");
             Assert.True(x.isTrue());
-            Assert.Equal(4, 2 + 2);
+
+            // Test using classes from cscore:
+            var json = JsonWriter.GetWriter().Write(x);
+            var x2 = JsonReader.GetReader().Read<MyClass1>(json);
+            Assert.Equal(x.name, x2.name);
         }
     }
+
 }
