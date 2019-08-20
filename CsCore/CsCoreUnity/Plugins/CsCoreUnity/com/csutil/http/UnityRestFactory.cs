5cfb8d4b794b32368a068fc7a6959e6b6aa72864
S <code@csutil.com>
2019-08-20 09:17:55 +0200

    Disabled Unity.Ping usage since its not supported on all platforms

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/http/UnityRestFactory.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/http/UnityRestFactory.cs
@@ -15,26 +15,26 @@ namespace com.csutil.http {
             throw new NotImplementedException("Not yet implemented for http method " + method);
         }
 
-        public override Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = 500) {
-            if (ApplicationV2.platform.IsAnyOf(RuntimePlatform.Android, RuntimePlatform.OSXPlayer, RuntimePlatform.WindowsPlayer)) {
-                // For Android and MacOs the Unity.Ping seems to not work for all types of ips and urls
-                return base.GetCurrentPing(ipOrUrl, timeoutInMs);
-            }
-            return GetCurrentPingViaUnity(ipOrUrl, timeoutInMs);
-        }
+        //public override Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = 500) {
+        //    if (ApplicationV2.platform.IsAnyOf(RuntimePlatform.Android, RuntimePlatform.OSXPlayer, RuntimePlatform.WindowsPlayer)) {
+        //        // For Android and MacOs the Unity.Ping seems to not work for all types of ips and urls
+        //        return base.GetCurrentPing(ipOrUrl, timeoutInMs);
+        //    }
+        //    return GetCurrentPingViaUnity(ipOrUrl, timeoutInMs);
+        //}
 
-        private Task<long> GetCurrentPingViaUnity(string ipOrUrl, int timeoutInMs) {
-            var p = new UnityEngine.Ping(ipOrUrl);
-            return WebRequestRunner.GetInstance(this).StartCoroutineAsTask<long>(UnityPingCoroutine(p, timeoutInMs), () => {
-                if (p.isDone) { return p.time; } else { return -1; }
-            });
-        }
+        //private Task<long> GetCurrentPingViaUnity(string ipOrUrl, int timeoutInMs) {
+        //    var p = new UnityEngine.Ping(ipOrUrl);
+        //    return WebRequestRunner.GetInstance(this).StartCoroutineAsTask<long>(UnityPingCoroutine(p, timeoutInMs), () => {
+        //        if (p.isDone) { return p.time; } else { return -1; }
+        //    });
+        //}
 
-        private System.Collections.IEnumerator UnityPingCoroutine(Ping ping, float timeoutInMs) {
-            float nrOfChecks = 30;
-            var wait = new WaitForSeconds(timeoutInMs / nrOfChecks / 1000f);
-            for (int i = 0; i < nrOfChecks && !ping.isDone; i++) { yield return wait; }
-        }
+        //private System.Collections.IEnumerator UnityPingCoroutine(UnityEngine.Ping ping, float timeoutInMs) {
+        //    float nrOfChecks = 30;
+        //    var wait = new WaitForSeconds(timeoutInMs / nrOfChecks / 1000f);
+        //    for (int i = 0; i < nrOfChecks && !ping.isDone; i++) { yield return wait; }
+        //}
     }
 
 }
