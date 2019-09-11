7cf224054bda27cc1586c105b50a79d81044a69f
S <code@csutil.com>
2019-09-11 06:08:44 +0200

    Added colors to the default console logger

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/logging/LogToConsole.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/logging/LogToConsole.cs
@@ -6,15 +6,22 @@ namespace com.csutil.logging {
     public class LogToConsole : LogDefaultImpl {
 
         protected override void PrintDebugMessage(string debugLogMsg, object[] args) {
-            Console.Write(debugLogMsg);
+            WithColor(ConsoleColor.DarkGray, () => { Console.Write(debugLogMsg); });
         }
 
         protected override void PrintErrorMessage(string errorMsg, object[] args) {
-            Console.Write(errorMsg);
+            WithColor(ConsoleColor.Red, () => { Console.Write(errorMsg); });
         }
 
         protected override void PrintWarningMessage(string warningMsg, object[] args) {
-            Console.Write(warningMsg);
+            WithColor(ConsoleColor.Yellow, () => { Console.Write(warningMsg); });
+        }
+
+        private void WithColor(ConsoleColor newForegroundColor, Action WriteToConsoleAction) {
+            var oldForegroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = newForegroundColor;
+            WriteToConsoleAction();
+            Console.ForegroundColor = oldForegroundColor;
         }
 
     }
