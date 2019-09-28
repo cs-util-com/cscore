789b41893a3f70ef31ddefc17497387046709b8d
S <code@csutil.com>
2019-09-28 19:05:38 +0200

    Extended the zip library code examples

--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ZipFileTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ZipFileTests.cs
@@ -1,9 +1,12 @@
+using System.IO;
 using ICSharpCode.SharpZipLib.Zip;
 using Xunit;
 
 namespace com.csutil.tests {
 
     public class ZipFileTests {
+
+        public ZipFileTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
 
         [Fact]
         public void ExampleUsage1() {
@@ -17,6 +20,7 @@ namespace com.csutil.tests {
 
             { // Save a test file in a sub sub dir:
                 dir1.GetChildDir("SubDir1").GetChild("t1.txt").SaveAsText("abc");
+                dir1.GetChildDir("SubDir2").GetChild("t2.txt").SaveAsText("def");
                 var fastZip = new FastZip();
                 fastZip.CreateZip(zip1.FullName, dir1.FullName, true, "");
             }
@@ -26,6 +30,17 @@ namespace com.csutil.tests {
             }
             Assert.True(dir2.ExistsV2());
             Assert.Equal("abc", dir2.GetChildDir("SubDir1").GetChild("t1.txt").LoadAs<string>());
+
+            { // Read content directly from zip without extraction:
+                var zip = new ZipFile(new FileStream(zip1.FullName, FileMode.Open, FileAccess.Read));
+                foreach (ZipEntry zipEntry in zip) { Log.d("entry: " + zipEntry.Name); }
+                Assert.Equal(2, zip.Count);
+                var e1 = zip.GetEntry("SubDir1/t1.txt");
+                Assert.True(e1.IsFile);
+                var streamReader = new StreamReader(zip.GetInputStream(e1));
+                var fileContent = streamReader.ReadToEnd();
+                Assert.Equal("abc", fileContent);
+            }
 
         }
 
