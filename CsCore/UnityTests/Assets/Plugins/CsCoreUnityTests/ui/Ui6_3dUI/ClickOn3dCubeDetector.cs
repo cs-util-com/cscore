e10224f2819e08135cc769fee561e96074751017
S <code@csutil.com>
2019-11-30 13:11:51 +0100

    Added clickable 3D object example in the 3d UI test scene

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityTests/ui/Ui6_3dUI/ClickOn3dCubeDetector.cs
@@ -0,0 +1,11 @@
+﻿using com.csutil;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickOn3dCubeDetector : MonoBehaviour, IPointerClickHandler {
+
+    public void OnPointerClick(PointerEventData eventData) {
+        Log.d("OnPointerClick on " + gameObject, gameObject);
+    }
+
+}
