10a85f9ab19ba41b2de4e208f01f2bb8c7e73343
S <code@csutil.com>
2020-01-26 16:49:04 -0500

    Added a new toggle component to switch between visible invisble state of a target GameObject and added a sample scene its usage

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/ToggleTargetVisibility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.csutil {
+
+    public class ToggleGoVisibility : MonoBehaviour, IPointerClickHandler {
+
+        public Transform target;
+
+        public void OnPointerClick(PointerEventData eventData) { ToggleVisibilityOfTarget(); }
+
+        public void ToggleVisibilityOfTarget() {
+            if (target == null) { throw Log.e("Toggle-target not set", gameObject); }
+            target.gameObject.SetActiveV2(!target.gameObject.activeSelf);
+        }
+
+    }
+
+}
