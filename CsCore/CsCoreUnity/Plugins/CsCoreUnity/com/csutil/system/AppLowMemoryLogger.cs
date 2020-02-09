d8cbd67509251d0ac383840b672a3b0b8f654d2c
S <code@csutil.com>
2020-02-09 12:28:21 +0100

    Added an framerate reducer logic that makes sure the FPS transitions to an idle state while the app is not interacted with

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/AppLowMemoryLogger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace com.csutil.system {
+
+    class AppLowMemoryLogger : MonoBehaviour {
+
+        private void OnEnable() { Application.lowMemory += LogLowMemory; }
+
+        private void OnDisable() { Application.lowMemory -= LogLowMemory; }
+
+        private void LogLowMemory() { AppFlow.TrackEvent(EventConsts.catError, "System reports: Low memory warning!"); }
+
+    }
+
+}
--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/ApplicationV2.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/ApplicationV2.cs
@@ -30,6 +30,31 @@ namespace com.csutil {
             }
         }
 
+        public static int targetFrameRateV2 {
+
+#if UNITY_2019_3_OR_NEWER
+            set {
+                // Start off assuming that Application.targetFrameRate is 60 and QualitySettings.vSyncCount is 0
+                OnDemandRendering.renderFrameInterval = 6;
+
+                // Some applications may allow the user to modify the quality level. So we may not be able to rely on
+                // the framerate always being a specific value. For this example we want the effective framerate to be value.
+                // If it is not then check the values and adjust the frame interval accordingly to achieve the framerate that we desire.
+                if (OnDemandRendering.effectiveRenderFrameRate != value) {
+                    if (QualitySettings.vSyncCount > 0) {
+                        OnDemandRendering.renderFrameInterval = (Screen.currentResolution.refreshRate / QualitySettings.vSyncCount / value);
+                    } else {
+                        OnDemandRendering.renderFrameInterval = (Application.targetFrameRate / value);
+                    }
+                }
+            }
+            get { return OnDemandRendering.effectiveRenderFrameRate; }
+#else
+            set { Application.targetFrameRate = value; }
+            get { return Application.targetFrameRate; }
+#endif
+        }
+
     }
 
 }
