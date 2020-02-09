22038a6355f27704714d4fbbdc30c5738bd3afd3
S <code@csutil.com>
2020-02-09 12:42:21 +0100

    Fixes in FPS logic for Unity 2019.3

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/ApplicationV2.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/ApplicationV2.cs
@@ -33,22 +33,20 @@ namespace com.csutil {
         public static int targetFrameRateV2 {
 
 #if UNITY_2019_3_OR_NEWER
+            // See: 
+            // - https://blogs.unity3d.com/2020/02/07/how-on-demand-rendering-can-improve-mobile-performance/
+            // - https://docs.unity3d.com/2019.3/Documentation/ScriptReference/Rendering.OnDemandRendering-effectiveRenderFrameRate.html
             set {
-                // Start off assuming that Application.targetFrameRate is 60 and QualitySettings.vSyncCount is 0
-                OnDemandRendering.renderFrameInterval = 6;
-
-                // Some applications may allow the user to modify the quality level. So we may not be able to rely on
-                // the framerate always being a specific value. For this example we want the effective framerate to be value.
-                // If it is not then check the values and adjust the frame interval accordingly to achieve the framerate that we desire.
-                if (OnDemandRendering.effectiveRenderFrameRate != value) {
+                UnityEngine.Rendering.OnDemandRendering.renderFrameInterval = 1;
+                if (UnityEngine.Rendering.OnDemandRendering.effectiveRenderFrameRate != value) {
                     if (QualitySettings.vSyncCount > 0) {
-                        OnDemandRendering.renderFrameInterval = (Screen.currentResolution.refreshRate / QualitySettings.vSyncCount / value);
+                        UnityEngine.Rendering.OnDemandRendering.renderFrameInterval = (Screen.currentResolution.refreshRate / QualitySettings.vSyncCount / value);
                     } else {
-                        OnDemandRendering.renderFrameInterval = (Application.targetFrameRate / value);
+                        UnityEngine.Rendering.OnDemandRendering.renderFrameInterval = (Application.targetFrameRate / value);
                     }
                 }
             }
-            get { return OnDemandRendering.effectiveRenderFrameRate; }
+            get { return UnityEngine.Rendering.OnDemandRendering.effectiveRenderFrameRate; }
 #else
             set { Application.targetFrameRate = value; }
             get { return Application.targetFrameRate; }
