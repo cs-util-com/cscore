2b5b4733ae00de167e01f27fe72efd38ca4b9134
S <code@csutil.com>
2020-02-15 10:47:28 +0100

    GetCurrentPing now also implemented for WebGL (uses head requests there instead of real Pings)

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/http/UnityRestFactory.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/http/UnityRestFactory.cs
@@ -16,15 +16,11 @@ namespace com.csutil.http {
             return new UnityRestRequest(new UnityWebRequest(uri, method.ToString()));
         }
 
-        public override async Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = 500) {
+        public override Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = 500) {
             if (ApplicationV2.platform.IsAnyOf(RuntimePlatform.WebGLPlayer)) {
-                var t1 = GetCurrentPingViaHeadRequest(ipOrUrl);
-                var t2 = GetCurrentPingViaUnity(ipOrUrl, timeoutInMs);
-                var quickerTask = await Task.WhenAny(t1, t2);
-                return await quickerTask;
+                return GetCurrentPingViaHeadRequest(ipOrUrl);
             }
-            // For Android and MacOs the Unity.Ping seems to not work for all types of ips and urls
-            return await base.GetCurrentPing(ipOrUrl, timeoutInMs);
+            return base.GetCurrentPing(ipOrUrl, timeoutInMs);
         }
 
         public Task<long> GetCurrentPingViaHeadRequest(string ipOrUrl = "8.8.8.8", int timeoutInMs = 500) {
@@ -42,20 +38,20 @@ namespace com.csutil.http {
             yield return UnityWebRequest.Head(ipOrUrl).SendWebRequestV2(resp);
         }
 
-        [Obsolete("UnityEngine.Ping does not exist in WebGL")]
-        private Task<long> GetCurrentPingViaUnity(string ipOrUrl, int timeoutInMs) {
-            var p = new UnityEngine.Ping(ipOrUrl);
-            return WebRequestRunner.GetInstance(this).StartCoroutineAsTask<long>(UnityPingCoroutine(p, timeoutInMs), () => {
-                if (p.isDone) { return p.time; } else { return -1; }
-            });
-        }
+        //[Obsolete("UnityEngine.Ping does not exist in WebGL")]
+        //private Task<long> GetCurrentPingViaUnity(string ipOrUrl, int timeoutInMs) {
+        //    var p = new UnityEngine.Ping(ipOrUrl);
+        //    return WebRequestRunner.GetInstance(this).StartCoroutineAsTask<long>(UnityPingCoroutine(p, timeoutInMs), () => {
+        //        if (p.isDone) { return p.time; } else { return -1; }
+        //    });
+        //}
 
-        [Obsolete("UnityEngine.Ping does not exist in WebGL")]
-        private System.Collections.IEnumerator UnityPingCoroutine(UnityEngine.Ping ping, float timeoutInMs) {
-            float nrOfChecks = 30;
-            var wait = new WaitForSeconds(timeoutInMs / nrOfChecks / 1000f);
-            for (int i = 0; i < nrOfChecks && !ping.isDone; i++) { yield return wait; }
-        }
+        //[Obsolete("UnityEngine.Ping does not exist in WebGL")]
+        //private System.Collections.IEnumerator UnityPingCoroutine(UnityEngine.Ping ping, float timeoutInMs) {
+        //    float nrOfChecks = 30;
+        //    var wait = new WaitForSeconds(timeoutInMs / nrOfChecks / 1000f);
+        //    for (int i = 0; i < nrOfChecks && !ping.isDone; i++) { yield return wait; }
+        //}
 
     }
 
