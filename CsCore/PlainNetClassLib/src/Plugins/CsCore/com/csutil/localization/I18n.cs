787ff03d58940b5226deb14de748d920821f2570
S <code@csutil.com>
2020-03-07 17:19:26 +0100

    Added inital traft of I18n translation system plus some tests and example usage

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/localization/I18n.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace com.csutil {
+
+    public class I18n { // Initial idea from https://github.com/MoonGateLabs/i18n-unity-csharp/
+
+        public static I18n instance(object caller) { return IoC.inject.Get<I18n>(caller); }
+
+        public bool isWarningLogEnabled = true;
+        public string currentLocale { get; private set; } = "en-US";
+        private CultureInfo cultureInfo;
+        private Func<string, string> localeLoader = TryLoadLocaleFromFile;
+        private JObject translationData;
+
+        public I18n SetLocale(string newLocale) {
+            currentLocale = newLocale;
+            cultureInfo = new CultureInfo(currentLocale);
+            LoadLocaleFile();
+            return this;
+        }
+
+        public I18n SetLocaleLoader(Func<string, string> newLocaleLoader) {
+            localeLoader = newLocaleLoader;
+            LoadLocaleFile();
+            return this;
+        }
+
+        private void LoadLocaleFile() {
+            if (localeLoader != null) { translationData = JObject.Parse(localeLoader(currentLocale)); }
+        }
+
+        public string Get(string key, params object[] args) {
+            string translation = key;
+            if (translationData?[key] != null) {
+                // if this key is a direct string
+                if (translationData[key].Count() == 0) {
+                    translation = "" + translationData[key];
+                } else {
+                    translation = FindSingularOrPlural(key, args);
+                }
+            } else if (isWarningLogEnabled) { Log.w("Could not translate: " + key); }
+            // check if we have embeddable data
+            if (args.Length > 0) {
+                translation = string.Format(cultureInfo, translation, args);
+            }
+            return translation;
+        }
+
+        string FindSingularOrPlural(string key, object[] args) {
+            var translationOptions = translationData[key];
+            string translation = key;
+            string singPlurKey;
+            // find format to try to use
+            switch (GetCountAmount(args)) {
+                case 0:
+                    singPlurKey = "zero";
+                    break;
+                case 1:
+                    singPlurKey = "one";
+                    break;
+                default:
+                    singPlurKey = "other";
+                    break;
+            }
+            // try to use this plural/singular key
+            if (translationOptions[singPlurKey] != null) {
+                translation = "" + translationOptions[singPlurKey];
+            } else if (isWarningLogEnabled) {
+                Log.w("Missing singPlurKey:" + singPlurKey + " for:" + key);
+            }
+            return translation;
+        }
+
+        int GetCountAmount(object[] args) {
+            int argOne = 0;
+            // If arguments passed, try to parse first one to use as count
+            if (args.Length > 0 && IsNumeric(args[0])) {
+                argOne = Math.Abs(Convert.ToInt32(args[0]));
+                if (argOne == 1 && Math.Abs(Convert.ToDouble(args[0])) != 1) { // Check if arg actually equals one
+                    argOne = 2;
+                } else { // Check if arg actually equals one:
+                    if (argOne == 0 && Math.Abs(Convert.ToDouble(args[0])) != 0) { argOne = 2; }
+                }
+            }
+            return argOne;
+        }
+
+        bool IsNumeric(object Expression) {
+            if (Expression == null || Expression is DateTime) { return false; }
+            if (Expression is short || Expression is int || Expression is long || Expression is decimal || Expression is float || Expression is double || Expression is bool) {
+                return true;
+            }
+            return false;
+        }
+
+        private static string TryLoadLocaleFromFile(string localeName) {
+            var dir = EnvironmentV2.instance.GetCurrentDirectory().GetChildDir("Locales");
+            if (!dir.ExistsV2()) { throw Log.e("Could not find locales file in " + dir); }
+            var f = dir.GetChild(localeName);
+            if (f.ExistsV2()) { return f.LoadAs<string>(); }
+            f = dir.GetChild(localeName + ".json");
+            if (f.ExistsV2()) { return f.LoadAs<string>(); }
+            throw Log.e("Can't load localization file: " + localeName);
+        }
+
+    }
+
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/text/I18nTests.cs
@@ -0,0 +1,141 @@
+using Xunit;
+
+namespace com.csutil.tests {
+
+    public class I18nTests {
+
+        public I18nTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public void ExampleUsage1() {
+            I18n i18n = NewI18nForTesting();
+            i18n.SetLocale("en-US");
+            Assert.Equal("Hello", i18n.Get("Hello"));
+
+            i18n.SetLocale("fr-FR");
+            Assert.Equal("Bonjour", i18n.Get("Hello"));
+
+            i18n.SetLocale("es-ES");
+            Assert.Equal("Hola", i18n.Get("Hello"));
+
+            i18n.SetLocale("de-DE");
+            Assert.Equal("de-DE", i18n.currentLocale);
+            Assert.Equal("Hallo", i18n.Get("Hello"));
+        }
+
+        [Fact]
+        public void ExampleUsage2() {
+            I18n i18n = NewI18nForTesting().SetLocale("en-US");
+            Assert.Equal("-1 credit", i18n.Get("{0} credits", -1));
+            Assert.Equal("15 credits", i18n.Get("{0} credits", 15));
+            Assert.Equal("15.23 credits", i18n.Get("{0} credits", 15.23));
+            Assert.Equal("-15 credits", i18n.Get("{0} credits", -15));
+            Assert.Equal("-15.23 credits", i18n.Get("{0} credits", -15.23));
+        }
+
+        [Fact]
+        public void TestDifferentLocales() {
+            I18n i18n = NewI18nForTesting().SetLocale("en-US");
+            Assert.Equal("You have a lot of cats!", i18n.Get("You have one cat", 11));
+            Assert.Equal("-0.34 credits", i18n.Get("{0} credits", -0.34));
+
+            Assert.Equal("No credits", i18n.Get("{0} credits", 0));
+            Assert.Equal("1 credit", i18n.Get("{0} credits", 1));
+            Assert.Equal("1.23 credits", i18n.Get("{0} credits", 1.23));
+
+            // Different locales use different number formatting:
+            i18n.SetLocale("de-DE");
+            Assert.Equal("Keine Credits", i18n.Get("{0} credits", 0));
+            Assert.Equal("1 Credit", i18n.Get("{0} credits", 1));
+            Assert.Equal("1,23 Credits", i18n.Get("{0} credits", 1.23));
+        }
+
+        [Fact]
+        public void TestReplacement() {
+            I18n i18n = NewI18nForTesting().SetLocale("en-US");
+            Assert.Equal("There are no monkeys in the tree.", i18n.Get("There is one monkey in the {1}", 0, "tree"));
+            Assert.Equal("There is one monkey in the tree.", i18n.Get("There is one monkey in the {1}", 1, "tree"));
+            Assert.Equal("There are 27 monkeys in the tree!", i18n.Get("There is one monkey in the {1}", 27, "tree"));
+            Assert.Equal("There are -5 monkeys in the tree!", i18n.Get("There is one monkey in the {1}", -5, "tree"));
+        }
+
+        [Fact]
+        public void TestComplexReplacement() {
+            I18n i18n = NewI18nForTesting().SetLocale("en-US");
+            var s1 = i18n.Get("Hello {0}, how are you today?", "Tester");
+            Assert.Equal("Hello Tester, how are you today?", s1);
+            var s2 = i18n.Get("Room {1}: time={0} score={3} ammo={4}, User {2}", "00:44:23", "A", "One", 100000, "12");
+            Assert.Equal("Room A: time=00:44:23 score=100000 ammo=12, User One", s2);
+        }
+
+        [Fact]
+        public void TestNoTranslation1() {
+            I18n i18n = NewI18nForTesting();
+            Assert.Equal("Hello World", i18n.Get("Hello World"));
+            Assert.Equal("1st place Mr. Potter", i18n.Get("{0}st place Mr. {1}", 1, "Potter"));
+        }
+
+        [Fact]
+        public void TestNoTranslation2() {
+            I18n i18n = new I18n().SetLocaleLoader(null).SetLocale("en-GB");
+            Assert.Equal("Hello World", i18n.Get("Hello World"));
+            Assert.Equal("1st place Mr. Potter", i18n.Get("{0}st place Mr. {1}", 1, "Potter"));
+        }
+
+        I18n NewI18nForTesting() { return new I18n().SetLocaleLoader(LoadTestLocales); }
+
+        private string LoadTestLocales(string localeName) {
+            if (localeName == "en-US") {
+                return @"
+                {
+                    'Hello': 'Hello',
+                    'Hello {0}, how are you today?': 'Hello {0}, how are you today?',
+                    'You have one cat': {
+                        'zero': 'You have no cats',
+                        'one': 'You have one cat',
+                        'other': 'You have a lot of cats!'
+                    },
+                    '{0} credits': {
+                        'zero': 'No credits',
+                        'one': '{0} credit',
+                        'other': '{0} credits'
+                    },
+                    'Level {1} time: {0} score:{3} ammo:{4} player:{2}': 'Level {1} time: {0} score:{3} ammo:{4} player:{2}',
+                    'There is one monkey in the {1}': {
+                        'zero': 'There are no monkeys in the {1}.',
+                        'one': 'There is one monkey in the {1}.',
+                        'other': 'There are {0} monkeys in the {1}!'
+                    }
+                }";
+            }
+            if (localeName == "de-DE") {
+                return @"
+                {
+                    'Hello': 'Hallo',
+                    '{0} credits': {
+                        'zero': 'Keine Credits',
+                        'one': '{0} Credit',
+                        'other': '{0} Credits'
+                    },
+                }";
+            }
+            if (localeName == "es-ES") {
+                return @"
+                {
+                    'Hello': 'Hola',
+                    'Hello %s, how are you today?': 'Hola %s, coma esta ahora?'
+                }";
+            }
+            if (localeName == "fr-FR") {
+                return @"
+                {
+                    'Hello': 'Bonjour',
+                    'Hello %s, how are you today?': 'Bonjour %s, comment allez-vous aujourd hui?'
+                }";
+            }
+            throw Log.e("locale not found");
+        }
+
+    }
+
+}
