a5a2ba3367ec7218d312f271afc061d1b37f4aeb
S <code@csutil.com>
2020-03-17 07:14:46 +0100

    Added FileSystem implementation to read from zip files the same way as normal folders can be accessed

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/io/ZioExtensions.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/io/ZioExtensions.cs
@@ -19,5 +19,37 @@ namespace com.csutil {
             return fs.GetDirectoryEntry(UPath.Root);
         }
 
+        /// <summary>
+        /// Gets the remaining path after the <see cref="prefix"/>.
+        /// </summary>
+        /// <param name="prefix">The prefix of the path.</param>
+        /// <param name="self">The path to search.</param>
+        /// <returns>The path after the prefix, or a <c>null</c> path if <see cref="path"/> does not have the correct prefix.</returns>
+        public static UPath RemovePrefix(this UPath self, UPath prefix) {
+            if (prefix.IsEmpty) { throw new InvalidDataException("The passed prefix cant be emtpy, must minimum be the UPath.root"); }
+            if (!self.IsInDirectory(prefix, true)) { throw new InvalidDataException($"Path '{self}' is not in '{prefix}'"); }
+            var remaining = self.FullName.Substring(prefix.FullName.Length);
+            return new UPath(remaining).ToAbsolute();
+        }
+
+        public static UPath AddPrefix(this UPath self, UPath prefix) {
+            return prefix.IsNull ? self : prefix / self.ToRelative();
+        }
+
+        /// <summary>
+        /// Gets the first directory of the specified path and return the remaining path (/a/b/c, first directory: /a, remaining: b/c)
+        /// </summary>
+        /// <param name="path">The path to extract the first directory and remaining.</param>
+        /// <param name="remainingPath">The remaining relative path after the first directory</param>
+        /// <returns>The first directory of the path.</returns>
+        /// <exception cref="ArgumentNullException">if path is <see cref="UPath.IsNull"/></exception>
+        public static UPath GetFirstPart(this UPath path) {
+            path.AssertNotNull();
+            var fullname = path.FullName;
+            var index = fullname.IndexOf(UPath.DirectorySeparator, 1);
+            if (index < 0) { return fullname; }
+            return fullname.Substring(0, index);
+        }
+
     }
 }
--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ZipFileTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ZipFileTests.cs
@@ -1,6 +1,9 @@
 using System.IO;
+using System.Linq;
+using com.csutil.io;
 using ICSharpCode.SharpZipLib.Zip;
 using Xunit;
+using Zio;
 
 namespace com.csutil.tests {
 
@@ -42,6 +45,44 @@ namespace com.csutil.tests {
                 Assert.Equal("abc", fileContent);
             }
 
+        }
+
+        [Fact]
+        public void ExampleUsage2() {
+
+            var root = EnvironmentV2.instance.GetOrAddTempFolder("ZipFileTests_ExampleUsage1");
+            root.DeleteV2(); // cleanup from previous test if needed
+
+            var dir1 = root.GetChildDir("Dir1");
+            var zip1 = root.GetChild("Test1.zip");
+
+            { // Save a test file in a sub sub dir:
+                dir1.GetChildDir("SubDir1").GetChild("t1.txt").SaveAsText("abc");
+                dir1.GetChildDir("SubDir2").GetChild("t2.txt").SaveAsText("def");
+                dir1.GetChildDir("SubDir2").GetChild("t3.txt").SaveAsText("ghi");
+                dir1.GetChildDir("SubDir3").GetChild("t4.txt").SaveAsText("jkl");
+                new FastZip().CreateZip(zip1.FullName, dir1.FullName, true, "");
+            }
+
+            DirectoryEntry zipContent = OpenZipReadOnly(zip1);
+            Assert.Equal(3, zipContent.EnumerateEntries().Count());
+
+            var subDir2 = zipContent.GetChildDir("/SubDir2");
+            Assert.Equal(2, subDir2.EnumerateEntries().Count());
+            Assert.Equal("def", subDir2.GetChild("t2.txt").LoadAs<string>());
+
+            zipContent.FileSystem.Dispose(); // Close the zip at the end
+
+        }
+
+        private static DirectoryEntry OpenZipReadOnly(FileInfo self) {
+            if (self == null) { throw new FileNotFoundException("Passed file was null"); }
+            if (!self.ExistsV2()) { throw new FileNotFoundException("Did not find any file at " + self); }
+            return OpenZipReadOnly(self.FullName);
+        }
+
+        private static DirectoryEntry OpenZipReadOnly(string path) {
+            return new ZipFileReadSystem(new ZipFile(path)).GetDirectoryEntry(UPath.Root);
         }
 
     }
