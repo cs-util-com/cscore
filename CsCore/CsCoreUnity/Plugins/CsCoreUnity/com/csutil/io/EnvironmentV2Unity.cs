d0118bb7c340aa3bf4804f9bc1796cac30c70989
S <code@csutil.com>
2020-03-20 09:51:44 +0100

    Some more fixes for webgl tests and image loading unit test

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/io/EnvironmentV2Unity.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/io/EnvironmentV2Unity.cs
@@ -10,6 +10,16 @@ namespace com.csutil.io {
 
     class EnvironmentV2Unity : EnvironmentV2 {
 
+        public bool isUnityEditor {
+            get {
+#if UNITY_EDITOR
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
         /// <summary>
         /// Base impl. would return C:\Users\User123\AppData\Local\Temp\ 
         /// Unity impl. will return C:\Users\User123\AppData\Local\Temp\DefaultCompany\UnityTestsA\
@@ -23,6 +33,10 @@ namespace com.csutil.io {
         }
 
         public override DirectoryEntry GetCurrentDirectory() {
+            if (EnvironmentV2.isWebGL && !isUnityEditor) {
+                AssertV2.IsTrue(Application.dataPath.StartsWith("http"), "Application.dataPath=" + Application.dataPath);
+                return GetRootAppDataFolder();
+            }
             return new DirectoryInfo(Application.dataPath).ToRootDirectoryEntry();
         }
 
--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ImageFileTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ImageFileTests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using StbImageLib;
 using Xunit;
 using Zio;
@@ -10,14 +12,25 @@ namespace com.csutil.tests {
         public ImageFileTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
 
         [Fact]
-        public void ExampleUsage1() {
+        public async Task ExampleUsage1() {
             var t = Log.MethodEntered();
-            var imgFile = EnvironmentV2.instance.GetCurrentDirectory().GetChild("testImage1.jpg");
+            FileEntry imgFile = await GetImageFile("testImage1.jpg");
             ImageResult image = LoadImage(imgFile);
             Assert.True(image.Height > 0);
             Assert.True(image.Width > 0);
             Log.MethodDone(t);
             Log.d("Image " + imgFile + " has size: " + image.Width + " x " + image.Height);
+        }
+
+        private static async Task<FileEntry> GetImageFile(string imageFileName) {
+            FileEntry imgFile = EnvironmentV2.instance.GetCurrentDirectory().GetChild(imageFileName);
+            if (!imgFile.Exists) { // If the file does not exist, download a random image and save it there:
+                var stream = await new Uri("https://picsum.photos/50/50").SendGET().GetResult<Stream>();
+                imgFile.SaveStream(stream);
+                stream.Dispose();
+                Log.e("Saved a random image for testing to " + imgFile.GetFullFileSystemPath());
+            }
+            return imgFile;
         }
 
         private static ImageResult LoadImage(FileEntry imgFile) {
