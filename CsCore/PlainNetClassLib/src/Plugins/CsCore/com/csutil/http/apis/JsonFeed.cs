d6f33a0bdeb30e3437dbe36d55704f5046b48f96
S <code@csutil.com>
2020-03-22 21:24:33 +0100

    Analog to the AtomFeed class tested also the JsonFeed class

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/http/apis/JsonFeed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace com.csutil.http.apis {
+
+    public static class JsonFeed {
+
+        /// <summary> A jsonfeed, e.g. https://hnrss.org/frontpage.jsonfeed </summary>
+        public static Task<Response> Get(Uri jsonFeedUrl) { return jsonFeedUrl.SendGET().GetResult<Response>(); }
+
+        public class Item {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string content_html { get; set; }
+            public string url { get; set; }
+            public string external_url { get; set; }
+            public DateTime date_published { get; set; }
+            public string author { get; set; }
+            public string content_text { get; set; }
+
+        }
+
+        public class Response {
+            public string version { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string home_page_url { get; set; }
+            public List<Item> items { get; set; }
+            public string feed_url { get; set; }
+            public string icon { get; set; }
+            public string favicon { get; set; }
+        }
+
+    }
+
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/http/apis/JsonFeedTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using com.csutil.http.apis;
+using Xunit;
+
+namespace com.csutil.tests.http {
+
+    public class JsonFeedTests {
+
+        public JsonFeedTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public async Task ExampleUsage1() {
+            var feed = await JsonFeed.Get(new Uri("https://hnrss.org/frontpage.jsonfeed"));
+            Assert.NotEmpty(feed.items);
+            foreach (var item in feed.items) { Log.d(item.title + " - " + item.url); }
+        }
+
+    }
+
+}
