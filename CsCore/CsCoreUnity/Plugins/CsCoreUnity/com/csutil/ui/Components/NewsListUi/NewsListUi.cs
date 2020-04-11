3cccd13614d4a983d55ca2f4a0f31da5c82d40f4
S <code@csutil.com>
2020-04-11 13:29:04 +0200

    Added a few Unity UI drafts to allow simple creation of the NewsManager UIs

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/NewsListUi/NewsListUi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using com.csutil.model;
+using com.csutil.system;
+using ReuseScroller;
+using UnityEngine;
+
+namespace com.csutil.ui {
+
+    public class NewsListUi : BaseController<News> {
+
+        public ColorDict colors = InitDefaultColors(); // Init with default color suggestions
+        protected NewsManager manager; // protected because its accessed by the list entries
+
+        public async Task LoadUnreadFrom(NewsManager manager) {
+            this.manager = manager;
+            var unreadNews = await manager.GetAllUnreadNews();
+            this.CellData = unreadNews.ToList(); // This will trigger showing the list entries
+        }
+
+        public Color GetColorFor(News item) {
+            if (colors.TryGetValue(item.GetNewsType(), out Color result)) { return result; }
+            return Color.white;
+        }
+
+        public virtual void OnItemClicked(News clickedItem) {
+            Application.OpenURL(clickedItem.detailsUrl);
+        }
+
+        [Serializable]
+        public class ColorDictEntry : SerializableEntry<News.NewsType, Color> { }
+        [Serializable]
+        public class ColorDict : SerializableDictionary<News.NewsType, Color, ColorDictEntry> { }
+
+        // Compact notation via dict and can be edited in inspector to adjust default values
+        private static ColorDict InitDefaultColors() {
+            var colors = new ColorDict(); // Serializable subclass of normal Dictionary
+            colors.Add(News.NewsType.Blog, ColorUtil.HexStringToColor("FBAE4E"));
+            colors.Add(News.NewsType.Announcement, ColorUtil.HexStringToColor("FFAE1B"));
+            colors.Add(News.NewsType.ComingSoon, ColorUtil.HexStringToColor("59D457"));
+            colors.Add(News.NewsType.Beta, ColorUtil.HexStringToColor("FF5A80"));
+            colors.Add(News.NewsType.New, ColorUtil.HexStringToColor("FF5A80"));
+            colors.Add(News.NewsType.Improvement, ColorUtil.HexStringToColor("71C4FF"));
+            colors.Add(News.NewsType.Warning, ColorUtil.HexStringToColor("EB5756"));
+            colors.Add(News.NewsType.Fix, ColorUtil.HexStringToColor("8482F5"));
+            return colors;
+        }
+
+    }
+
+}
---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/NewsListUi/NewsListUiEntry.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using com.csutil.system;
+using ReuseScroller;
+using UnityEngine.UI;
+
+namespace com.csutil.ui {
+
+    class NewsListUiEntry : BaseCell<News> {
+
+        public Text dateText;
+        public Text typeText;
+        public Text titleText;
+        public Text descrText;
+        public Text detailsUrlText;
+        public Graphic typeIndicator;
+        public Image image;
+        public Image thumbnail;
+        public Button titleButton;
+        public Button detailsUrlButton;
+        private NewsListUi newsListUi;
+
+        public override void UpdateContent(News item) {
+
+            if (newsListUi == null) { newsListUi = GetComponentInParent<NewsListUi>(); }
+
+            typeIndicator.color = newsListUi.GetColorFor(item);
+            SetText(typeText, item.type);
+
+            LoadImageFromUrl(image, item.imageUrl);
+            LoadImageFromUrl(thumbnail, item.thumbnailUrl);
+
+            SetText(dateText, item.GetDate().ToReadableString());
+            SetText(titleText, item.title);
+            SetText(descrText, item.description);
+
+            SetText(detailsUrlText, item.detailsUrlText);
+
+            detailsUrlButton.gameObject.SetActiveV2(!item.detailsUrl.IsNullOrEmpty());
+            if (!item.detailsUrl.IsNullOrEmpty()) {
+                detailsUrlButton.SetOnClickAction(_ => newsListUi.OnItemClicked(item));
+                titleButton.SetOnClickAction(_ => newsListUi.OnItemClicked(item));
+            }
+
+        }
+
+        private void LoadImageFromUrl(Image targetImage, string urlToLoad) {
+            AssertV2.NotNull(targetImage, "targetImage");
+            var isUrlEmpty = urlToLoad.IsNullOrEmpty();
+            targetImage.gameObject.GetParent().SetActiveV2(!isUrlEmpty);
+            if (!isUrlEmpty) { targetImage.LoadFromUrl(urlToLoad).OnError(LogImageLoadError); }
+        }
+
+        private Task LogImageLoadError(Exception e) { Log.w("Image Load Error: " + e); return Task.FromException(e); }
+
+        private static void SetText(Text target, string text) {
+            if (target == null) { return; }
+            target.gameObject.SetActiveV2(!text.IsNullOrEmpty());
+            target.text = text;
+        }
+
+    }
+
+}
