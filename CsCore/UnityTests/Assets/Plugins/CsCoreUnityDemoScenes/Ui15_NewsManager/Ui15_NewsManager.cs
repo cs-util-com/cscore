e6429563a40df48d3623eea2b3cd491bcf858d75
S <code@csutil.com>
2020-04-11 13:29:55 +0200

    Added a new UI example that uses the new NewsManager UI templates and loads from an example Google sheet its news data

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui15_NewsManager/Ui15_NewsManager.cs
@@ -0,0 +1,30 @@
+﻿using com.csutil.system;
+using System.Collections;
+using System.Threading.Tasks;
+using com.csutil.ui;
+
+namespace com.csutil.tests {
+
+    public class Ui15_NewsManager : UnitTestMono {
+
+        public override IEnumerator RunTest() {
+
+            // Get your key from https://console.developers.google.com/apis/credentials
+            var apiKey = "AIzaSyCtcFQMgRIUHhSuXggm4BtXT4eZvUrBWN0";
+            // See https://docs.google.com/spreadsheets/d/1Hwu4ZtRR0iXD65Wuj_XyJxLw4PN8SE0sRgnBKeVoq3A
+            var sheetId = "1Hwu4ZtRR0iXD65Wuj_XyJxLw4PN8SE0sRgnBKeVoq3A";
+            var sheetName = "MySheet1"; // Has to match the sheet name
+
+            NewsManager manager = NewsManager.NewManagerViaGSheets(apiKey, sheetId, sheetName);
+            yield return LoadAllNews(manager).AsCoroutine();
+
+        }
+
+        private async Task LoadAllNews(NewsManager manager) {
+            var map = gameObject.GetLinkMap();
+            await map.Get<NewsListUi>("NewsListUi").LoadUnreadFrom(manager);
+        }
+
+    }
+
+}
