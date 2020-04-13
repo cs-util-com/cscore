5ca4ee39c85a90df1adbfc0d4d0d57164060a600
S <code@csutil.com>
2020-04-13 10:54:28 +0200

    Started drafting an input form UI example

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui16_InputForm/Ui16_InputForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.csutil.tests {
+
+    public class Ui16_InputForm : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
+}
