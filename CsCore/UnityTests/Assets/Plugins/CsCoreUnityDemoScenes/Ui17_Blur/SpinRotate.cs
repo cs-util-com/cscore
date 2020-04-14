73987db45dca783d6035144057c7651ca1b2c655
S <code@csutil.com>
2020-04-14 09:26:38 +0200

    Added a test scene for the blur shader

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui17_Blur/SpinRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace com.csutil.tests {
+
+    public class SpinRotate : MonoBehaviour {
+
+        public Vector3 axis = Vector3.up;
+        public float speed = 10;
+
+        void Update() { transform.Rotate(axis, speed * Time.deltaTime); }
+
+    }
+
+}
