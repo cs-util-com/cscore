4f34bd5ed30af58af51ef5cca5536f09c3e9da5d
S <code@csutil.com>
2020-05-17 16:40:11 +0200

    Added an EnumField prefab and EnumFieldView (which is a special type of InputFieldView) to allow the user to select from predefind values (e.g from an enum)

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/mtvmtv/EnumFieldView.cs
@@ -0,0 +1,33 @@
+﻿using com.csutil.model.mtvmtv;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.csutil.ui.mtvmtv {
+
+    public class EnumFieldView : InputFieldView {
+
+        public GameObject menuGo;
+        public GameObject menuEntriesContainer;
+        public GameObject menuEntryPrefab;
+
+        protected override async Task Setup(string fieldName, string fullPath) {
+            await base.Setup(fieldName, fullPath);
+            input.interactable = field.additionalItems == true;
+            CreateMenuEntryUis();
+        }
+
+        private void CreateMenuEntryUis() {
+            foreach (string e in field.contentEnum) {
+                var entry = menuEntriesContainer.AddChild(Instantiate(menuEntryPrefab));
+                entry.GetComponentInChildren<Text>().textLocalized(e);
+                entry.GetComponentInChildren<Button>().SetOnClickAction(delegate {
+                    input.SetTextWithNotify(e);
+                    menuGo.SetActiveV2(false); // Hide menu
+                });
+            }
+        }
+
+    }
+
+}
