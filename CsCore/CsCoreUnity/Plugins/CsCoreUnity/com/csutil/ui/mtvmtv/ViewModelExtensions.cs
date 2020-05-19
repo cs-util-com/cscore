5057d4f6eabb232c308e5ba8e4bc5410d5da25b0
S <code@csutil.com>
2020-05-19 09:30:23 +0200

    Some docu added

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/mtvmtv/ViewModelExtensions.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/mtvmtv/ViewModelExtensions.cs
@@ -55,13 +55,21 @@ namespace com.csutil.ui.mtvmtv {
             return self;
         }
 
-        public static async Task<T> LoadViaJsonIntoView<T>(this Presenter<JObject> self, T model, Task userSavesChanges) {
+        /// <summary> 
+        /// Converts the passed model to JSON, lets the user edit it and returned a parsed back clone with all changes 
+        /// made by the user, so that this new state can be stored or the changed fields can be calculated via MergeJson.GetDiff()
+        /// </summary>
+        /// <param name="model"> The model that should be shown in the UI (has to fit the loaded view model UI) </param>
+        /// <param name="userSavedChanges"> 
+        /// A task that should be set to completed once the user is finished with the UI, e.g. when he presses the save button 
+        /// </param>
+        /// <returns> The modified model after the passed userSavedChanges-Task is completed </returns>
+        public static async Task<T> LoadViaJsonIntoView<T>(this Presenter<JObject> self, T model, Task userSavedChanges) {
             JObject json = JObject.Parse(JsonWriter.GetWriter().Write(model));
             await self.LoadModelIntoView(json);
-            await userSavesChanges;
+            await userSavedChanges;
             return JsonReader.GetReader().Read<T>(json.ToString());
         }
-
 
         public static bool IsInChildObject(this FieldView self) { return self.fieldName != self.fullPath; }
 
