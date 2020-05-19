2033482e891c9946c7e5686e39aa2fc650d37dc9
S <code@csutil.com>
2020-05-19 09:20:06 +0200

    Added info to log if log is printed from edit mode

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/logging/LogToUnityDebugLog.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/logging/LogToUnityDebugLog.cs
@@ -8,19 +8,26 @@ namespace com.csutil.logging {
     public class LogToUnityDebugLog : LogDefaultImpl {
 
         protected override void PrintDebugMessage(string debugLogMsg, params object[] args) {
-            Debug.Log(debugLogMsg, GetGoFrom(args));
+            Debug.Log(IsEditMode() + debugLogMsg, GetGoFrom(args));
         }
 
         protected override void PrintWarningMessage(string warningMsg, params object[] args) {
-            Debug.LogWarning(warningMsg, GetGoFrom(args));
+            Debug.LogWarning(IsEditMode() + warningMsg, GetGoFrom(args));
         }
 
         protected override void PrintErrorMessage(string errorMsg, params object[] args) {
-            Debug.LogError(errorMsg, GetGoFrom(args));
+            Debug.LogError(IsEditMode() + errorMsg, GetGoFrom(args));
         }
 
         protected override void PrintException(Exception e, params object[] args) {
             Debug.LogException(e, GetGoFrom(args));
+        }
+
+        private string IsEditMode() {
+#if UNITY_EDITOR
+            if (!Application.isPlaying) { return "[ EDIT MODE ]  "; }
+#endif
+            return "";
         }
 
         protected override string ArgToString(object arg) {
