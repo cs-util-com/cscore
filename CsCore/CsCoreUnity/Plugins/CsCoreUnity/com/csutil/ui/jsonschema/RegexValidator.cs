60a4cb56bb32111b5a736bb0c5b89c3d73f9765b
S <code@csutil.com>
2020-06-07 09:39:32 +0200

    Added docu and smaller refactorings inthe Regex Validator

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/jsonschema/RegexValidator.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/jsonschema/RegexValidator.cs
@@ -5,29 +5,41 @@ using UnityEngine.UI;
 
 namespace com.csutil.ui.mtvmtv {
 
+    /// <summary> A regex validator that evaluates a target input field and informs the user if 
+    /// the entered data does not comply with its regex </summary>
     public class RegexValidator : MonoBehaviour {
 
+        /// <summary> The regular expression that is used to evaluate the input </summary>
         public string regex;
+        /// <summary> The target input that is monitored for changes and compared to the regex </summary>
         public InputField inputToValidate;
         /// <summary> If true then the input cant be null/emtpy </summary>
         public bool isInputRequired = false;
+        /// <summary> The UI that is set to visible when an invalid input is detected </summary>
         public GameObject errorUi;
+        /// <summary> The error text that the UI shows to the user when an invalid input is detected </summary>
         public Text errorText;
+        /// <summary> The detail that is waited before the latest input is evaluated, 
+        /// so that intermediate inputs are skipped while the user is still typing </summary>
         public double validationDelayInMs = 500;
 
         /// <summary> True if the current input matches the set regex </summary>
         private bool isInputValid = true;
-        private Color? originalColor;
+        private Color? cachedOriginalColor;
         private UnityAction<string> inputListener;
+
+        private void OnEnable() {
+            inputListener = inputToValidate.AddOnValueChangedActionThrottled(EvalNewValue, validationDelayInMs);
+        }
+
+        private void OnDisable() {
+            if (inputListener != null) { inputToValidate.onValueChanged.RemoveListener(inputListener); }
+        }
 
         public void EnforceRegex(string regex) {
             if (enabled) { enabled = false; }
             this.regex = regex;
             enabled = true;
-        }
-
-        private void OnEnable() {
-            inputListener = inputToValidate.AddOnValueChangedActionThrottled(EvalNewValue, validationDelayInMs);
         }
 
         public bool CheckIfCurrentInputValid() {
@@ -53,17 +65,13 @@ namespace com.csutil.ui.mtvmtv {
             if (inputToValidate.targetGraphic != null) {
                 if (!wasLatestCheckValid) {
                     if (errorText.color != inputToValidate.targetGraphic.color) {
-                        originalColor = inputToValidate.targetGraphic.color;
+                        cachedOriginalColor = inputToValidate.targetGraphic.color;
                         inputToValidate.targetGraphic.color = errorText.color;
                     }
-                } else if (originalColor != null) {
-                    inputToValidate.targetGraphic.color = originalColor.Value;
+                } else if (cachedOriginalColor != null) {
+                    inputToValidate.targetGraphic.color = cachedOriginalColor.Value;
                 }
             }
-        }
-
-        private void OnDisable() {
-            if (inputListener != null) { inputToValidate.onValueChanged.RemoveListener(inputListener); }
         }
 
         public static bool IsAllInputCurrentlyValid(GameObject view) {
