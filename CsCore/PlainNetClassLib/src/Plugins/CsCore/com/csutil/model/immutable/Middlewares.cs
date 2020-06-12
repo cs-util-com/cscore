6e2143d5355bdc78f79c7449feb5f1895679075e
S <code@csutil.com>
2020-06-12 14:29:12 +0200

    Added check in redux that actions ensure that they are not modified by dispatching them to ensure they do not carry any state

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/Middlewares.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/Middlewares.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using JsonDiffPatchDotNet;
 using Newtonsoft.Json.Linq;
 
@@ -34,32 +35,44 @@ namespace com.csutil.model.immutable {
         public static Middleware<T> NewLoggingMiddleware<T>() {
             return (store) => {
                 Log.MethodEntered("NewLoggingMiddleware", "store =" + store);
-                return (Dispatcher innerDispatcher) => {
-                    Dispatcher loggingDispatcher = (action) => {
-                        if (action is IsValid v && !v.IsValid()) {
-                            Log.e("Invalid action: " + asJson(action.GetType().Name, action));
-                        }
-                        T previousState = store.GetState();
-                        var returnedAction = innerDispatcher(action);
-                        T newState = store.GetState();
-                        if (Object.Equals(previousState, newState)) {
-                            Log.w("The action  " + action + " was not handled by any of the reducers! Store=" + store);
-                        } else {
-                            ShowChanges(action, previousState, newState);
-                        }
-                        return returnedAction;
-                    };
-                    return loggingDispatcher;
-                };
+                return (Dispatcher innerDispatcher) => NewLoggingDispatcher(store, innerDispatcher);
             };
         }
 
+        private static Dispatcher NewLoggingDispatcher<T>(IDataStore<T> store, Dispatcher innerDispatcher) {
+            return (action) => {
+                if (action is IsValid v && !v.IsValid()) {
+                    Log.e("Invalid action: " + asJson(action.GetType().Name, action));
+                }
+                // If the action is a delegate it will be handled by thunk and cant be copied:
+                var actionBeforeDispatch = action is Delegate ? null : action.DeepCopy();
+                T previousState = store.GetState();
+                var returnedAction = innerDispatcher(action);
+                T newState = store.GetState();
+                if (actionBeforeDispatch != null) { AssertActionDidNotChangeDuringDispatch(actionBeforeDispatch, action); }
+                if (Object.Equals(previousState, newState)) {
+                    Log.w("The action  " + action + " was not handled by any of the reducers! Store=" + store);
+                } else {
+                    ShowChanges(action, previousState, newState);
+                }
+                return returnedAction;
+            };
+        }
+
+        [Conditional("DEBUG"), Conditional("ENFORCE_FULL_LOGGING")]
+        private static void AssertActionDidNotChangeDuringDispatch(object actionBeforeDispatch, object actionAfter) {
+            JToken diff = MergeJson.GetDiff(actionBeforeDispatch, actionAfter);
+            if (!diff.IsNullOrEmpty()) {
+                throw Log.e("The action was changed by dispatching it, check reducers: " + diff.ToPrettyString());
+            }
+        }
+
+        [Conditional("DEBUG"), Conditional("ENFORCE_FULL_LOGGING")]
         private static void ShowChanges<T>(object action, T previousState, T newState) {
             try {
                 JToken diff = MergeJson.GetDiff(previousState, newState);
                 Log.d(asJson("" + action.GetType().Name, action), asJson("previousState -> newState diff", diff));
-            }
-            catch (Exception e) { Log.e(e); }
+            } catch (Exception e) { Log.e(e); }
         }
 
         private static string asJson(string varName, object result) { return varName + "=" + JsonWriter.AsPrettyString(result); }
