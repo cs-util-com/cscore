e5073a021e86b2956bf705b1f1bbf0183c5afcf9
S <code@csutil.com>
2020-06-12 17:29:06 +0200

    Added a second usage test for diffing 2 images and displaying how much they differ in a result image

--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ImageMagickTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ImageMagickTests.cs
@@ -15,6 +15,26 @@ namespace com.csutil.tests {
         public static void LoadFromFileEntry(this MagickImage self, FileEntry f) {
             using (Stream inputStream = f.OpenForRead()) { self.Read(inputStream); }
         }
+
+        public static double Compare(this MagickImage self, MagickImage newImg, out MagickImage diffImg, ErrorMetric errorMetric = ErrorMetric.MeanSquared) {
+            diffImg = new MagickImage();
+            return self.Compare(newImg, new CompareSettings() { Metric = errorMetric }, diffImg);
+        }
+
+        public static void Compare(this MagickImage self, FileEntry imgFile, double MAX_ALLOWED_DIFF = 0.001) {
+            using (MagickImage oldImg = new MagickImage()) {
+                oldImg.LoadFromFileEntry(imgFile);
+                FileEntry diffFile = imgFile.Parent.GetChild(imgFile.NameWithoutExtension + "_diff" + imgFile.ExtensionWithDot);
+                if (oldImg.Compare(self, out MagickImage imgWithDifferences) > MAX_ALLOWED_DIFF) {
+                    Log.e("Detected high difference vs old image, see diffFile: " + diffFile);
+                } else {
+                    self.SaveToFileEntry(imgFile);
+                }
+                imgWithDifferences.SaveToFileEntry(diffFile);
+                imgWithDifferences.Dispose();
+            }
+        }
+
     }
 
     public class ImageMagickTests {
@@ -38,6 +58,36 @@ namespace com.csutil.tests {
             }
             Log.MethodDone(t);
             Log.d("Saved to " + outputFile.GetFullFileSystemPath());
+        }
+
+        [Fact]
+        public async Task TestImgDiffing() {
+            FileEntry imFileToDiff_original = await GetImageFile("imFileToDiff_original.jpg");
+            FileEntry imFileToDiff_modified = imFileToDiff_original.Parent.GetChild("imFileToDiff_sharpened.jpg");
+
+            var t = Log.MethodEntered("Load modify save compare save diff");
+
+            if (!imFileToDiff_modified.IsNotNullAndExists()) {
+                GenerateModifiedVariant(imFileToDiff_original, imFileToDiff_modified);
+            }
+
+            using (MagickImage newImg = new MagickImage()) {
+                newImg.LoadFromFileEntry(imFileToDiff_modified);
+                newImg.Compare(imFileToDiff_original);
+            }
+
+            Log.MethodDone(t);
+            Log.d("Saved to " + imFileToDiff_modified.GetFullFileSystemPath());
+        }
+
+        private static void GenerateModifiedVariant(FileEntry imFileToDiff_original, FileEntry imFileToDiff_sharpened) {
+            using (MagickImage original = new MagickImage()) {
+                original.LoadFromFileEntry(imFileToDiff_original);
+                using (MagickImage sharpened = new MagickImage(original)) {
+                    sharpened.Sharpen(); // Create a sharpened version of the original image
+                    sharpened.SaveToFileEntry(imFileToDiff_sharpened);
+                }
+            }
         }
 
         private static async Task<FileEntry> GetImageFile(string imageFileName) {
