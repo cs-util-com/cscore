a96f4528624af3189642c59953ee3ce0039d7a85
S <code@csutil.com>
2020-07-14 08:56:14 +0200

    Added a Ui21_UserInput example scene to use the 2 new input controllers

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui21_UserInput/Ui21_UserInput.cs
@@ -0,0 +1,26 @@
+﻿using com.csutil.ui;
+using System.Collections;
+
+namespace com.csutil.tests {
+
+    public class Ui21_UserInput : UnitTestMono {
+
+        public override IEnumerator RunTest() {
+            var input = GetComponentInChildren<PointerInputHandler>();
+            input.onClick.AddListener(() => Log.d("onClick"));
+            input.onDoubleClick.AddListener(() => Log.d("onDoubleClick"));
+            input.onLongPressStart.AddListener(() => {
+                Log.d("onLongPressStart");
+                Toast.Show("Now drag me around");
+                input.gameObject.AddComponent<UiDragHandler>();
+            });
+            input.onLongPressEnd.AddListener(() => {
+                Log.d("onLongPressEnd");
+                input.gameObject.GetComponent<UiDragHandler>().Destroy();
+            });
+            yield return null;
+        }
+
+    }
+
+}
