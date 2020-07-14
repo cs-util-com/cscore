af65c855be1647a7ba968e0c3f3f1a28264ef576
S <code@csutil.com>
2020-07-14 08:51:26 +0200

    Improved the Unity news manager example to use the local analytics system and a few usage rules to add based on these rules local news events to the news manager

--- CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui15_NewsManager/Ui15_NewsManager.cs
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui15_NewsManager/Ui15_NewsManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Threading.Tasks;
 using com.csutil.ui;
 using com.csutil.keyvaluestore;
+using com.csutil.logging.analytics;
+using com.csutil.model.usagerules;
 
 namespace com.csutil.tests {
 
@@ -25,16 +27,30 @@ namespace com.csutil.tests {
         }
 
         private static async Task AddAFewLocalNewsEvents(InMemoryKeyValueStore onDeviceEventsStore) {
+
+            var a = new LocalAnalytics();
+            // Set up a few simple usage rules to generate local news events from if they are true:
+            var appUsed1DayRule = a.NewAppUsedXDaysRule(days: 1);
+            var featureNeverUsedRule = a.NewFeatureNotUsedXTimesRule("feature1", times: 1);
+
+            var appNotUsedLast5Days = a.NewAppUsedInTheLastXDaysRule(5);
+            var userCameBackRule = a.NewConcatRule(appUsed1DayRule, appNotUsedLast5Days);
+
             var url = "https://github.com/cs-util-com/cscore";
             var urlText = "Show details..";
-            var n = News.NewLocalNewsEvent("Achivement Unlocked", "You just started the app", url, urlText);
-            await onDeviceEventsStore.Set(n.key, n);
-            await TaskV2.Delay(10);
-            var n2 = News.NewLocalNewsEvent("Feature X now available", "You can now do X", url, urlText);
-            await onDeviceEventsStore.Set(n2.key, n2);
-            await TaskV2.Delay(10);
-            var n3 = News.NewLocalNewsEvent("Feature Y now available", "You can now do Y", url, urlText);
-            await onDeviceEventsStore.Set(n3.key, n3);
+
+            if (await appUsed1DayRule.isTrue()) {
+                var n = News.NewLocalNewsEvent("Achivement Unlocked", "You used the app 1 day", url, urlText);
+                await onDeviceEventsStore.Set(n.key, n);
+            }
+            if (await featureNeverUsedRule.isTrue()) {
+                var n = News.NewLocalNewsEvent("Did you know you can do feature1?", "Feature 1 is the best", url, urlText);
+                await onDeviceEventsStore.Set(n.key, n);
+            }
+            if (await userCameBackRule.isTrue()) {
+                var n = News.NewLocalNewsEvent("You did not use the app for a while", "How dare you", url, urlText);
+                await onDeviceEventsStore.Set(n.key, n);
+            }
         }
 
         private async Task LoadAllNews(NewsManager manager) {
