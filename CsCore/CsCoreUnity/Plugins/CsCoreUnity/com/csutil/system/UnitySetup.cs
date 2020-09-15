3681f1dd01b2b96597e9503529a132139330d1c2
S <code@csutil.com>
2020-09-15 08:09:46 +0200

    Improved unity initialization setup logic that is triggered before each scene execution to reset all static singletons etc

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/UnitySetup.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/system/UnitySetup.cs
@@ -8,41 +8,34 @@ using UnityEngine;
 
 namespace com.csutil {
 
-    public class UnitySetup {
+    public static class UnitySetup {
 
         public const string UNITY_SETUP_DONE = "Unity setup now done";
+
+        static UnitySetup() { // This method is only executed only once after recompile 
+            // Debug.Log("com.csutil.UnitySetup static constructor called..");
+        }
 
 #if UNITY_2019_3_OR_NEWER // <- Check needed? TODO
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
 #endif  // See https://blogs.unity3d.com/2019/11/05/enter-play-mode-faster-in-unity-2019-3/
         public static void ResetAllStaticObjects() {
-            Debug.Log("ResetAllStaticObjects");
+            // Debug.Log("ResetAllStaticObjects");
             EventBus.instance = new EventBus();
             IoC.inject = Injector.newInjector(EventBus.instance);
             Log.instance = new LogToUnityDebugLog();
-        }
-
-        static UnitySetup() { // This method is only executed only once at the very beginning 
-            // Debug.Log("com.csutil.UnitySetup static constructor called..");
+            var caller = new object();
+            IoC.inject.SetSingleton<EnvironmentV2>(new EnvironmentV2Unity(), true);
+            if (EnvironmentV2.isWebGL) { IoC.inject.SetSingleton<TaskV2>(new TaskV2WebGL(), true); }
+            IoC.inject.SetSingleton<RestFactory>(new UnityRestFactory(), true);
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void BeforeSceneLoad() {
-            Debug.Log("BeforeSceneLoad");
+            // Debug.Log("BeforeSceneLoad");
             SystemConsoleToUnityLogRedirector.Setup();
             try { DestroyExistingMainThreadIfNeeded(); } catch (Exception e) { Debug.Log(e); }
-            SetupDefaultSingletonsIfNeeded();
-        }
-
-        public static void SetupDefaultSingletonsIfNeeded() {
             MainThread.instance.enabled = true; // Called to init main thread if not yet done by other logic
-            var caller = new object();
-            Log.instance = IoC.inject.GetOrAddSingleton<ILog>(caller, () => new LogToUnityDebugLog());
-            IoC.inject.GetOrAddSingleton<EnvironmentV2>(caller, () => new EnvironmentV2Unity());
-            IoC.inject.GetOrAddSingleton<RestFactory>(caller, () => new UnityRestFactory());
-            if (EnvironmentV2.isWebGL) {
-                IoC.inject.GetOrAddSingleton<TaskV2>(caller, () => new TaskV2WebGL());
-            }
         }
 
         private static void DestroyExistingMainThreadIfNeeded() {
