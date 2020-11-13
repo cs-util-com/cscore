23746912ac964c748f39af8a97f63a9a045af844
S <code@csutil.com>
2020-11-13 09:50:00 +0100

    Its now possible to ask a pointer event from the input system for its local postion on the target gameobject

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/extensions/PointerEventDataExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.csutil {
+
+    public static class PointerEventDataExtensions {
+
+        public static Vector2 localPosition(this PointerEventData e) {
+            var rt = e.pointerEnter.GetComponent<RectTransform>();
+            if (rt != null) {
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, e.position, e.pressEventCamera, out Vector2 result);
+                return result;
+            } else {
+                return e.pointerEnter.transform.InverseTransformPoint(e.pointerCurrentRaycast.worldPosition);
+            }
+        }
+
+        public static bool IsLeftClick(this PointerEventData self) { return self.button == PointerEventData.InputButton.Left; }
+
+        public static bool IsRightClick(this PointerEventData self) { return self.button == PointerEventData.InputButton.Right; }
+
+    }
+
+}
