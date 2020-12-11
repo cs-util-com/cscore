236ea52e0f5e3838ab5b81c766770fa6dbce6a26
S <code@csutil.com>
2020-12-11 21:34:58 +0100

    Introduced a new helper class I called ResourceCache which allows the developer to link to Resources that will be required by the ResourcesV2.Load() logic This allows to use ResourcesV2.Load on Resources that are not placed in a resource folder but instead are linked in such a ResourceCache so that the Cache can be placed e.g. in the scene and this way define which Resources will be required to be included in the build if the scene is part of the build

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/io/ResourceCache.cs
@@ -0,0 +1,63 @@
+﻿using com.csutil.model;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace com.csutil.io {
+
+    public class ResourceCache : MonoBehaviour {
+
+        public static bool TryLoad<T>(string pathInResourcesFolder, out T result) {
+            var caches = ResourcesV2.FindAllInScene<ResourceCache>();
+            foreach (var cache in caches) {
+                if (cache.Find(pathInResourcesFolder, out T res)) {
+                    result = res;
+                    return true;
+                }
+            }
+            result = default(T);
+            return false;
+        }
+
+        [Serializable]
+        public class ObjDictEntry : SerializableEntry<string, UnityEngine.Object> {
+        }
+        [Serializable]
+        public class ObjDict : SerializableDictionary<string, UnityEngine.Object, ObjDictEntry> { }
+
+        public ObjDict cache;
+
+        private bool Find<T>(string pathInResourcesFolder, out T res) {
+            if (cache != null) {
+                if (cache.TryGetValue(pathInResourcesFolder, out UnityEngine.Object r)) {
+                    if (r is T t) {
+                        res = t;
+                        return true;
+                    } else {
+                        Log.e($"Entry with correct pathInResourcesFolder='{pathInResourcesFolder}' was " +
+                            $"found but type was {r.GetType()} instead of {typeof(T)}");
+                    }
+                }
+            }
+            res = default(T);
+            return false;
+        }
+
+        private void OnValidate() {
+            if (cache == null) {
+                gameObject.name = nameof(ResourceCache);
+                cache = new ObjDict();
+            }
+            var emtpyOnes = cache.Filter(o => o.Key.IsNullOrEmpty() && o.Value != null).ToList();
+            foreach (var x in emtpyOnes) {
+                cache.Add(CalcKey(x.Value), x.Value);
+                cache.Remove(x.Key);
+            }
+        }
+
+        /// <summary> Tries to guess the correct pass the cached object would have in the Resources folder </summary>
+        private string CalcKey(UnityEngine.Object value) { return "" + value.name; }
+
+    }
+
+}
