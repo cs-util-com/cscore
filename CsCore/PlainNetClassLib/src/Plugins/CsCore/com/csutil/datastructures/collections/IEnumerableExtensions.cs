e96ea719670aa003ad7a6180118b0c02497b38c4
S <code@csutil.com>
2021-01-14 07:29:44 +0100

    Added a ienumberable extension method that caches an ienumerable so that it can be traversed multiple times, needed eg for yield enumerables if multiple traversales are required. See https://stackoverflow.com/a/12428250/165106

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/datastructures/collections/IEnumerableExtensions.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/datastructures/collections/IEnumerableExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -73,6 +74,74 @@ namespace com.csutil {
             if (self.Any(x => { index++; return predicate(x); })) { return index; }
             return -1;
         }
+
+        public static IEnumerable<T> Cached<T>(this IEnumerable<T> source) {
+            if (source == null) { throw new ArgumentNullException("source"); }
+            return new CachedEnumerable<T>(source);
+        }
+
+        /// <summary> Caches an ienumerable so that it can be traversed multiple times, needed eg for 
+        /// yield enumerables if multiple traversales are required. See https://stackoverflow.com/a/12428250/165106 </summary>
+        private class CachedEnumerable<T> : IEnumerable<T> {
+
+            readonly Object gate = new Object();
+            readonly IEnumerable<T> source;
+            readonly List<T> cache = new List<T>();
+            IEnumerator<T> enumerator;
+            bool isCacheComplete;
+
+            public CachedEnumerable(IEnumerable<T> source) {           this.source = source;            }
+
+            public IEnumerator<T> GetEnumerator() {
+                lock (this.gate) {
+                    if (this.isCacheComplete)
+                        return this.cache.GetEnumerator();
+                    if (this.enumerator == null)
+                        this.enumerator = source.GetEnumerator();
+                }
+                return GetCacheBuildingEnumerator();
+            }
+
+            public IEnumerator<T> GetCacheBuildingEnumerator() {
+                var index = 0;
+                T item;
+                while (TryGetItem(index, out item)) {
+                    yield return item;
+                    index += 1;
+                }
+            }
+
+            bool TryGetItem(Int32 index, out T item) {
+                lock (this.gate) {
+                    if (!IsItemInCache(index)) {
+                        // The iteration may have completed while waiting for the lock.
+                        if (this.isCacheComplete) {
+                            item = default(T);
+                            return false;
+                        }
+                        if (!this.enumerator.MoveNext()) {
+                            item = default(T);
+                            this.isCacheComplete = true;
+                            this.enumerator.Dispose();
+                            return false;
+                        }
+                        this.cache.Add(this.enumerator.Current);
+                    }
+                    item = this.cache[index];
+                    return true;
+                }
+            }
+
+            bool IsItemInCache(Int32 index) {
+                return index < this.cache.Count;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                return GetEnumerator();
+            }
+
+        }
+
 
     }
 
