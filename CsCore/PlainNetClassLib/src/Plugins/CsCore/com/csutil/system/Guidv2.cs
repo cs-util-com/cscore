c6f90fd37281ae7c73f2ad4d328902b67c00eb50
S <code@csutil.com>
2021-01-14 07:43:23 +0100

    Replaced sequential UUID generation algo with a better version also used in entity framework code ( the SequentialGuidValueGenerator )

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/system/Guidv2.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/system/Guidv2.cs
@@ -1,51 +1,48 @@
 ﻿using System;
+using System.Threading;
 
 namespace com.csutil {
 
     public static class Guidv2 {
 
-		private static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
+        private static long _counter = DateTimeV2.UtcNow.Ticks;
 
-		/// <summary> 
-		/// Generate a new <see cref="Guid"/> using the comb algorithm. 
-		/// 
-		/// The <c>comb</c> algorithm is designed to make the use of GUIDs as Primary Keys, Foreign Keys, 
-		/// and Indexes nearly as efficient as ints.
-		/// 
-		/// The <c>comb</c> algorithm is designed to make the use of GUIDs as Primary Keys, Foreign Keys, 
-		/// and Indexes nearly as efficient as ints.
-		/// 
-		/// This code was suggested in Jimmy Nilsson's 
-		/// <a href="http://www.informit.com/articles/article.asp?p=25862">article</a>
-		/// on <a href="http://www.informit.com">informit.com</a> and contributed by Donald Mull.
-		/// 
-		/// Source: https://stackoverflow.com/a/25472825/165106
-		/// </summary>
-		public static Guid NewGuidSequentialAndPartiallyOrdered() {
-			byte[] guidArray = Guid.NewGuid().ToByteArray();
+        /// <summary> 
+        ///     <para>
+        ///         Generates sequential <see cref="Guid" /> values optimized for use in Microsoft SQL server clustered
+        ///         keys or indexes, yielding better performance than random values. This is the default generator for
+        ///         SQL Server <see cref="Guid" /> columns which are set to be generated on add.
+        ///     </para>
+        ///     <para>
+        ///         See https://docs.microsoft.com/sql/t-sql/functions/newsequentialid-transact-sql.
+        ///         Although this generator achieves the same goals as SQL Server's NEWSEQUENTIALID, the algorithm used
+        ///         to generate the GUIDs is different.
+        ///     </para>
+        ///     <para>
+        ///         The generated values are non-temporary, meaning they will be saved to the database.
+        ///     </para>
+        ///     Source: https://github.com/dotnet/efcore/blob/main/src/EFCore/ValueGeneration/SequentialGuidValueGenerator.cs
+        /// </summary>
+        public static Guid NewGuidSequentialAndPartiallyOrdered() {
+            var guidBytes = Guid.NewGuid().ToByteArray();
+            var counterBytes = BitConverter.GetBytes(Interlocked.Increment(ref _counter));
 
-			DateTime now = DateTimeV2.UtcNow;
+            if (!BitConverter.IsLittleEndian) {
+                Array.Reverse(counterBytes);
+            }
 
-			// Get the days and milliseconds which will be used to build the byte string 
-			TimeSpan days = new TimeSpan(now.Ticks - BaseDateTicks);
-			TimeSpan msecs = now.TimeOfDay;
+            guidBytes[08] = counterBytes[1];
+            guidBytes[09] = counterBytes[0];
+            guidBytes[10] = counterBytes[7];
+            guidBytes[11] = counterBytes[6];
+            guidBytes[12] = counterBytes[5];
+            guidBytes[13] = counterBytes[4];
+            guidBytes[14] = counterBytes[3];
+            guidBytes[15] = counterBytes[2];
 
-			// Convert to a byte array 
-			// Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333 
-			byte[] daysArray = BitConverter.GetBytes(days.Days);
-			byte[] msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds / 3.333333));
+            return new Guid(guidBytes);
+        }
 
-			// Reverse the bytes to match SQL Servers ordering 
-			Array.Reverse(daysArray);
-			Array.Reverse(msecsArray);
-
-			// Copy the bytes into the guid 
-			Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-			Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
-
-			return new Guid(guidArray);
-		}
-
-	}
+    }
 
 }
