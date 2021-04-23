e0b9ccf32fb30aa7046a689b89e008a322bdb4f0
S <code@csutil.com>
2021-04-23 08:34:16 +0200

    Added a few example classes to show some less known c# features (using the code as documentation how to use them)

---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/csharp/GotoExample.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace com.csutil.tests {
+
+    public class GotoExample {
+
+        public GotoExample(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public void Example1() {
+            var i = 0;
+            StartOfMyLoop:
+            i++;
+            if (i < 5) { goto StartOfMyLoop; }
+            Assert.Equal(5, i);
+        }
+
+    }
+
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/csharp/UsingWithRenameExample.cs
@@ -0,0 +1,18 @@
+﻿using Do = System.Diagnostics.Debug;
+using Xunit;
+
+namespace com.csutil.tests {
+
+    public class UsingWithRenameExample {
+
+        public UsingWithRenameExample(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public void Example1() {
+            // In this file the System.Diagnostics.Debug class can be used as 'Do':
+            Do.Assert(3 == 1 + 2);
+        }
+
+    }
+
+}
