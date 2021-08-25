24d8af9111e4245d74aac6199b69bb69ba9a9452
S <code@csutil.com>
2021-08-25 08:41:12 +0200

    Changed default ping timeout from 1 sec to 1.5 sec

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/http/RestFactory.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/http/RestFactory.cs
@@ -7,20 +7,22 @@ namespace com.csutil.http {
 
     public class RestFactory {
 
+        public const int DEFAULT_PING_TIMEOUT = 1500;
+
         public static RestFactory instance { get { return IoC.inject.GetOrAddSingleton<RestFactory>(new object()); } }
 
         public virtual RestRequest SendRequest(Uri uri, HttpMethod method) {
             return new UriRestRequest(uri).Send(method);
         }
 
-        public virtual async Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = 1000) {
+        public virtual async Task<long> GetCurrentPing(string ipOrUrl = "8.8.8.8", int timeoutInMs = DEFAULT_PING_TIMEOUT) {
             PingReply pingReply = await new Ping().SendPingAsync(ipOrUrl, timeoutInMs);
             AssertV2.IsNotNull(pingReply, "pingReply");
             if (pingReply.Status != IPStatus.Success) { throw new TimeoutException("Ping failed: " + pingReply.Status); }
             return pingReply.RoundtripTime; // return ping in MS
         }
 
-        public async Task<bool> HasInternet(Action hasInet = null, Action noInet = null, string ip = "8.8.8.8", int timeoutMs = 1000) {
+        public async Task<bool> HasInternet(Action hasInet = null, Action noInet = null, string ip = "8.8.8.8", int timeoutMs = DEFAULT_PING_TIMEOUT) {
             var ping = await GetCurrentPing(ip, timeoutMs);
             if (ping >= 0) {
                 hasInet.InvokeIfNotNull();
