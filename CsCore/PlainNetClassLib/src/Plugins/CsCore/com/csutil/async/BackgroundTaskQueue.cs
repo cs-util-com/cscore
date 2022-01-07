4662e335f6f0136c94655a87306734d33b5129f7
S <code@csutil.com>
2022-01-07 08:14:28 +0100

    Added a background task queue that uses a shared scheduler and a cancelation token when creating new background tasks

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/async/BackgroundTaskQueue.cs
@@ -0,0 +1,76 @@
+﻿using com.csutil.progress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
+
+namespace com.csutil {
+
+    public interface IBackgroundTaskQueue {
+        TaskScheduler Scheduler { get; }
+        CancellationTokenSource Cancel { get; }
+        ICollection<Task> Tasks { get; }
+        IProgress ProgressListener { set; get; }
+    }
+
+    public static class TaskQueueExtensions {
+
+        public static int GetCompletedTasksCount(this IBackgroundTaskQueue self) { return self.Tasks.Filter(t => t.IsCompleted).Count(); }
+        public static int GetRemainingScheduledTaskCount(this IBackgroundTaskQueue self) { return self.Tasks.Filter(t => !t.IsCompleted).Count(); }
+        public static int GetTotalTasksCount(this IBackgroundTaskQueue self) { return self.Tasks.Count; }
+
+        public static async Task WhenAllTasksCompleted(this IBackgroundTaskQueue self, int millisecondsDelay = 50, bool flushQueueAfterCompletion = false) {
+            await Task.WhenAll(self.Tasks);
+            if (millisecondsDelay > 0) { await TaskV2.Delay(millisecondsDelay); }
+            // If there were new tasks added in the meantime, do another wait for the remaining tasks:
+            if (!self.IsAllTasksCompleted()) { await self.WhenAllTasksCompleted(); }
+            self.Tasks.Clear(); // Automatically flush task queue after all are done?
+        }
+
+        public static bool IsAllTasksCompleted(this IBackgroundTaskQueue self) { return self.GetRemainingScheduledTaskCount() == 0; }
+
+        public static void CancelAllOpenTasks(this IBackgroundTaskQueue self) {
+            if (!self.Cancel.IsCancellationRequested) { self.Cancel.Cancel(); }
+        }
+
+        public static async Task Run(this IBackgroundTaskQueue self, Func<Task> asyncAction) {
+            var t = TaskV2.Run(asyncAction, self.Cancel, self.Scheduler);
+            await AddToManagedTasks(self, t);
+        }
+
+        public static async Task<T> Run<T>(this IBackgroundTaskQueue self, Func<Task<T>> asyncFunction) {
+            var t = TaskV2.Run(asyncFunction, self.Cancel, self.Scheduler);
+            await AddToManagedTasks(self, t);
+            return await t;
+        }
+
+        private static async Task AddToManagedTasks(IBackgroundTaskQueue self, Task taskToAdd) {
+            self.Tasks.Add(taskToAdd);
+            self.ProgressListener?.SetCount(self.GetCompletedTasksCount(), self.GetTotalTasksCount());
+            await taskToAdd; // After the task is done update the progress listener again:
+            self.ProgressListener?.SetCount(self.GetCompletedTasksCount(), self.GetTotalTasksCount());
+        }
+
+    }
+
+    public class BackgroundTaskQueue : IBackgroundTaskQueue {
+
+        public static IBackgroundTaskQueue NewBackgroundTaskQueue(int maxConcurrencyLevel) {
+            return new BackgroundTaskQueue(new QueuedTaskScheduler(TaskScheduler.Default, maxConcurrencyLevel), new CancellationTokenSource());
+        }
+
+        public TaskScheduler Scheduler { get; }
+        public CancellationTokenSource Cancel { get; }
+        ICollection<Task> IBackgroundTaskQueue.Tasks { get; } = new List<Task>();
+        IProgress IBackgroundTaskQueue.ProgressListener { get; set; }
+
+        public BackgroundTaskQueue(QueuedTaskScheduler scheduler, CancellationTokenSource cancelTokenSource) {
+            Scheduler = scheduler;
+            Cancel = cancelTokenSource;
+        }
+
+    }
+
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/async/BackgroundTaskQueueTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace com.csutil.tests.async {
+
+    [Collection("Sequential")] // Will execute tests in here sequentially
+    public class BackgroundTaskQueueTests {
+
+        public BackgroundTaskQueueTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public async Task TestRunWithTaskScheduler1() { // Aligned with the coroutine test TestExecuteRepeated1
+
+            IBackgroundTaskQueue taskQueue = BackgroundTaskQueue.NewBackgroundTaskQueue(maxConcurrencyLevel: 1);
+
+            // Create both tasks at the same time:
+            Task t1 = taskQueue.Run(SomeAsyncTask1);
+            Task<string> t2 = taskQueue.Run(SomeAsyncTask2);
+
+            Assert.Equal(2, taskQueue.GetRemainingScheduledTaskCount());
+            Assert.Equal(0, taskQueue.GetCompletedTasksCount());
+            Assert.Equal(2, taskQueue.GetTotalTasksCount());
+            await t1;
+
+            Assert.Equal(1, taskQueue.GetRemainingScheduledTaskCount()); // 1 task started and 1 waiting
+            Assert.Equal(1, taskQueue.GetCompletedTasksCount()); // 1 task started and 1 waiting
+            Assert.Equal(2, taskQueue.GetTotalTasksCount());
+            Assert.Equal("Some string result", await t2);
+
+            Assert.Equal(0, taskQueue.GetRemainingScheduledTaskCount());
+            Assert.Equal(2, taskQueue.GetCompletedTasksCount());
+            Assert.Equal(2, taskQueue.GetTotalTasksCount());
+
+            // Since the scheduler allows only one task at a time, if t2 is done, t1 also must be completed:
+            Assert.True(t1.IsCompleted);
+
+        }
+
+        [Fact]
+        public async Task TestRunWithTaskScheduler2() { // Aligned with the coroutine test TestExecuteRepeated1
+
+            IBackgroundTaskQueue taskQueue = BackgroundTaskQueue.NewBackgroundTaskQueue(maxConcurrencyLevel: 2);
+
+            // Create both tasks at the same time:
+            Task t1 = taskQueue.Run(SomeAsyncTask1);
+            Task<string> t2 = taskQueue.Run(SomeAsyncTask2);
+            var t3 = taskQueue.Run(SomeAsyncTask1); // Add a 3rd task (will not be started)
+            Assert.Equal(3, taskQueue.GetRemainingScheduledTaskCount());
+
+            await t2;
+            // Since the scheduler allows 2 tasks at a time, t1 will not be complete when t2 is done:
+            Assert.False(t1.IsCompleted);
+            Assert.Equal(1, taskQueue.GetCompletedTasksCount());
+
+            await taskQueue.WhenAllTasksCompleted(flushQueueAfterCompletion: true);
+            Assert.Equal(0, taskQueue.GetRemainingScheduledTaskCount());
+            Assert.Equal(0, taskQueue.GetCompletedTasksCount()); // Queue was flushed
+
+        }
+
+        private async Task SomeAsyncTask1() {
+            var t = Log.MethodEntered();
+            await TaskV2.Delay(500);
+            Log.MethodDone(t);
+        }
+
+        private async Task<string> SomeAsyncTask2() {
+            var t = Log.MethodEntered();
+            await TaskV2.Delay(5);
+            Log.MethodDone(t);
+            return "Some string result";
+        }
+
+    }
+
+}
