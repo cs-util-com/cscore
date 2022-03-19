8e7f1a2cefacd2286ca36046d954939cd9a70af2
S <code@csutil.com>
2022-03-19 11:36:27 +0100

    Adding a test to reproduce the character limit problem of 259 characters in Unity under Windows

--- CsCore/UnityTests/Assets/Plugins/CsCoreUnityTests/io/FileTests.cs
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityTests/io/FileTests.cs
@@ -21,6 +21,70 @@ namespace com.csutil.tests.io {
             Assert.IsNotEmpty(dir.FullName);
         }
 
+        [Test]
+        public void TestLongPathes01() {
+            var dir1 = CreateDirectoryForTesting("TestLongPathes");
+            dir1 = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            dir1.CreateV2();
+            Assert.True(dir1.Exists);
+            var file = dir1.GetChild("XXXX.jpg");
+            using (var s = file.OpenOrCreateForWrite()) { }
+            Assert.True(file.Exists);
+        }
+
+        [Test]
+        public void TestLongPathes02() {
+            var dir1 = CreateDirectoryForTesting("TestLongPathes");
+            var subDir = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            subDir.CreateV2();
+            Assert.True(subDir.Exists);
+            var file = subDir.GetChild("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.jpg");
+            using (var s = file.OpenOrCreateForWrite()) { }
+            Assert.True(file.Exists);
+        }
+
+        [Test]
+        public void TestLongPathes03() {
+            var dir1 = CreateDirectoryForTesting("TestLongPathes");
+            var subDir = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            subDir.CreateV2();
+            Assert.True(subDir.Exists);
+            var file = subDir.GetChild("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.jpg");
+            using (var s = file.OpenOrCreateForWrite()) { }
+            Assert.True(file.Exists);
+        }
+
+        [Test]
+        public void TestLongPathes04() {
+            var dir1 = CreateDirectoryForTesting("TestLongPathes");
+            dir1 = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            dir1 = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            dir1.CreateV2();
+            Assert.True(dir1.Exists);
+            var file = dir1.GetChild("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.jpg");
+            using (var s = file.OpenOrCreateForWrite()) { }
+            Assert.True(file.Exists);
+        }
+
+        [Test]
+        public void TestLongPathes10() {
+            var dir1 = CreateDirectoryForTesting("TestLongPathes");
+            var subDir = dir1.GetChildDir("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            subDir.CreateV2();
+            Assert.True(subDir.Exists);
+            var file = subDir.GetChild("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.jpg");
+            using (var s = file.OpenOrCreateForWrite()) { }
+            Assert.True(file.Exists);
+        }
+
+        private static DirectoryEntry CreateDirectoryForTesting(string name) {
+            var rootDir = EnvironmentV2.instance.GetRootTempFolder().GetChildDir(name);
+            rootDir.DeleteV2(); // ensure that the test folder does not yet exist
+            rootDir.CreateV2();
+            Assert.True(rootDir.Exists);
+            return rootDir;
+        }
+
     }
 
 }
