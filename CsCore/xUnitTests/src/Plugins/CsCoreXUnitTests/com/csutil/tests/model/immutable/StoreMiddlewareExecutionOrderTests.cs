de08c1c4c5e089d18d2df8955ebda02dbd8d5bad
S <code@csutil.com>
2022-05-14 14:49:24 +0200

    Added a test that ensures that Actions dispatched from a middleware do not arrive in the incorrect order

--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/model/immutable/StoreMiddlewareExecutionOrderTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/model/immutable/StoreMiddlewareExecutionOrderTests.cs
@@ -10,7 +10,7 @@ namespace com.csutil.tests.model.immutable {
         public StoreMiddlewareExecutionOrderTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
 
         [Fact]
-        public async Task TestDispatchInListenerCallback() {
+        public void TestDispatchInListenerCallback() {
 
             var store = new DataStore<MyModel>(ReduceMyModel, new MyModel(0), NewMiddleware());
 
@@ -27,6 +27,50 @@ namespace com.csutil.tests.model.immutable {
             Assert.Equal(4, store.GetState().val);
         }
 
+        [Fact]
+        public void TestDispatchInMiddleware() {
+            DataStore<MyModel> store = null;
+            bool flag1 = true;
+            bool flag2 = false;
+            // Simulate that the middleware for some events Dispatches actions itself:
+            var middleware = NewMiddlewareThatDispatchesEvents(() => {
+                if (flag1) {
+                    flag1 = false;
+                    store.Dispatch(new MyAction() { val = 2 });
+                }
+                if (flag2) {
+                    flag2 = false;
+                    store.Dispatch(new MyAction() { val = 4 });
+                }
+            });
+            store = new DataStore<MyModel>(ReduceMyModel, new MyModel(0), middleware);
+
+            int counter = 0;
+            { // Ensure that in the listener never an older state arrives aver a newer state:
+                int val = store.GetState().val;
+                store.AddStateChangeListener(x => x.val, newVal => {
+                    Log.d($"{val} => {newVal}");
+                    counter++;
+                    if (val >= newVal) {
+                        throw new InvalidOperationException($"{val} vs {newVal}");
+                    }
+                    val = newVal;
+                }, triggerInstantToInit: false);
+            }
+
+            store.Dispatch(new MyAction() { val = 1 });
+            Assert.Equal(2, store.GetState().val);
+            // Even though there are 2 actions dispatched the listener is only triggered once since the
+            // state does not change between the first and second listener call. Both actions already have
+            // been send through the Reducers:
+            Assert.Equal(1, counter);
+
+            flag2 = true;
+            store.Dispatch(new MyAction() { val = 3 });
+            Assert.Equal(4, store.GetState().val);
+            Assert.Equal(2, counter);
+        }
+
         private class MyModel {
             public readonly int val;
             public MyModel(int val) { this.val = val; }
@@ -39,7 +83,7 @@ namespace com.csutil.tests.model.immutable {
         private MyModel ReduceMyModel(MyModel previousState, object action) {
             if (action is MyAction a) {
                 if (previousState.val + 1 != a.val) {
-                    throw new InvalidOperationException($"{previousState.val} vs {a.val}");
+                    throw new InvalidOperationException($"ReduceMyModel: {previousState.val} vs {a.val}");
                 }
                 return new MyModel(a.val);
             }
@@ -58,6 +102,23 @@ namespace com.csutil.tests.model.immutable {
                             throw new InvalidOperationException($"{val} vs {store.GetState().val}");
                         }
                         val = store.GetState().val;
+                        return action;
+                    };
+                };
+            };
+        }
+
+        private Middleware<MyModel> NewMiddlewareThatDispatchesEvents(Action onDispatch) {
+            int val = 0;
+            return (store) => {
+                return (innerDispatcher) => {
+                    return (action) => {
+                        innerDispatcher(action);
+                        if (val + 1 != store.GetState().val) {
+                            throw new InvalidOperationException($"{val} vs {store.GetState().val}");
+                        }
+                        val = store.GetState().val;
+                        onDispatch.InvokeIfNotNull();
                         return action;
                     };
                 };
