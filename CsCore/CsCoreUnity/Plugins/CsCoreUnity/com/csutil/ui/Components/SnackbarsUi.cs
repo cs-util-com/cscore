402d1f6b3569150a49fd7298b83625962cb20fd5
S <code@csutil.com>
2022-07-18 09:36:12 +0200

    Smaller fixes for Snackbars to allow using them as tutorial instructions

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/SnackbarsUi.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/SnackbarsUi.cs
@@ -40,11 +40,13 @@ namespace com.csutil {
             map.Get<Text>("Message").text = snackbarMsg;
             if (snackbarAction != null && !buttonMsg.IsNullOrEmpty()) {
                 map.Get<Text>("SnackbarButton").text = buttonMsg;
-                map.Get<Button>("SnackbarButton").SetOnClickAction(snackbarAction);
+                map.Get<Button>("SnackbarButton").SetOnClickAction(delegate { snackbarAction(newSnackbar); });
             } else {
                 map.Get<GameObject>("SnackbarButton").Destroy();
             }
-            newSnackbar.GetComponentV2<MonoBehaviour>().ExecuteDelayed(() => newSnackbar.Destroy(), displayDurationInMs);
+            if (displayDurationInMs > 0) {
+                newSnackbar.GetComponentV2<MonoBehaviour>().ExecuteDelayed(() => newSnackbar.Destroy(), displayDurationInMs);
+            }
             snackbarsContainer.AddChild(newSnackbar);
             return newSnackbar;
         }
