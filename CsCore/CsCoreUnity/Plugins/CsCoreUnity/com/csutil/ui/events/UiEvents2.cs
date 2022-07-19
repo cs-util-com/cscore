12b2b011ab6b1b04fe8910e987d2d5290b6ebb75
S <code@csutil.com>
2022-07-19 09:47:21 +0200

    Moved UiEvents class to its own folder

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/events/UiEvents2.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/events/UiEvents2.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace com.csutil.ui {
+﻿namespace com.csutil.ui {
 
     public static class UiEvents {
 
