304ce4a84e28452d9af764596806c7421ac1fb3a
S <code@csutil.com>
2022-08-13 07:56:28 +0200

    Improved the implementation of dispatching async tasks to the Unity MainThread

--- CsCore/UnityTests/Assets/Plugins/CsCoreUnityTests/async/TaskTests.cs
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityTests/async/TaskTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -12,6 +13,22 @@ namespace com.csutil.tests.threading {
             Assert.IsTrue(MainThread.instance.enabled);
             MainThread.Invoke(() => { Assert.IsTrue(Application.isPlaying); });
             yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestAsyncTaskOnMainThread() {
+            yield return TestAsyncTaskOnMainThreadAsync().AsCoroutine();
+        }
+
+        private async Task TestAsyncTaskOnMainThreadAsync() {
+            var b1 = false;
+            await MainThread.Invoke(async () => {
+                await TaskV2.Delay(100);
+                b1 = true;
+            });
+            Assert.True(b1);
+
+            Assert.AreEqual("abc", await MainThread.Invoke(async () => "abc"));
         }
 
     }
