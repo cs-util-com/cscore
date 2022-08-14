86464e8c0ab746eac43b4d974ac0c5d28691e709
S <code@csutil.com>
2022-08-14 13:38:54 +0200

    Added the PromiseMap pattern described at https://news.ycombinator.com/item?id=32189416
    The PromiseMap represents a caching pattern where instead of having a key:value cache instead key:promise-that-resolves-to-value—doing is used, this gives you
    dog piling prevention for free, because the first lookup of a value triggers the computation to fetch it while subsequent lookups wait on the same promise to
    resolve—or resolve instantly if the computation has completed

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/datastructures/collections/PromiseMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace com.csutil {
+
+    /// <summary> The PromiseMap represents a caching pattern where instead of having a key:value cache instead key:promise-that-resolves-to-value—doing is used, this gives you
+    /// dog piling prevention for free, because the first lookup of a value triggers the computation to fetch it while subsequent lookups wait on the same promise to
+    /// resolve—or resolve instantly if the computation has completed. See https://news.ycombinator.com/item?id=32189416 for further discussion of this pattern </summary>
+    public class PromiseMap<TKey, TValue> : ConcurrentDictionary<TKey, Task<TValue>> {
+
+    }
+
+}
