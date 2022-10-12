9079a61e1044ffa87440393f2843ef0e2073f252
S <code@csutil.com>
2022-10-12 06:40:47 +0200

    Added docu to the gps math methods and added an example usage xunit test

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/math/IHasLatLong.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/math/IHasLatLong.cs
@@ -19,13 +19,24 @@ namespace com.csutil.gps {
 
         private const double Deg2Rad = Math.PI / 180d;
 
-        public static void CalcGpsCoords(this IHasLatLong zeroPoint, double xDistInMeters, double yDistInMeters, double[] resultLatLong) {
+        /// <summary> Calculates GPS coordinates of a relative point that lives in the coordinate space of the <see cref="zeroPoint"/> </summary>
+        /// <param name="zeroPoint"> The reference GPS position the relative coordinates live in </param>
+        /// <param name="eastDistInMeters"> Distance in meters on axis pointing to east </param>
+        /// <param name="northDistInMeters"> Distance in meters on axis pointing to north </param>
+        /// <param name="resultLatLong"> Contains latitude (on position 0) and longitude (on position 1) </param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void CalcGpsCoords(this IHasLatLong zeroPoint, double eastDistInMeters, double northDistInMeters, double[] resultLatLong) {
             if (resultLatLong.Length != 2) { throw new ArgumentException("Length of passed result array did not have the correct length 2"); }
             double vall = longi2rad * Math.Cos(zeroPoint.Latitude * Deg2Rad);
-            resultLatLong[0] = yDistInMeters / lati2rad + zeroPoint.Latitude;
-            resultLatLong[1] = xDistInMeters / vall + zeroPoint.Longitude;
+            resultLatLong[0] = northDistInMeters / lati2rad + zeroPoint.Latitude;
+            resultLatLong[1] = eastDistInMeters / vall + zeroPoint.Longitude;
         }
 
+        /// <summary> Calculates the relative point (with distance to <see cref="zeroPoint"/> in meters) based on the passed in (latitude,longitude) values so
+        /// that it can be shown in a 3D coordinate system </summary>
+        /// <param name="self"> The GPS coordinates to convert to local coordinates </param>
+        /// <param name="resultCoordsInMeters"> A vector with 2 values where value[0] is the distance on the axis pointing east and value[1] is the distance on the axis pointing north </param>
+        /// <param name="zeroPoint"> The reference GPS point that is used for all conversions to local 3D space </param>
         public static void CalcRelativeCoordsInMeters(this IHasLatLong self, double[] resultCoordsInMeters, IHasLatLong zeroPoint) {
             if (resultCoordsInMeters.Length != 2) { throw new ArgumentException("Length of passed result array did not have the correct length 2"); }
             double vall = longi2rad * Math.Cos(zeroPoint.Latitude * Deg2Rad);
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/model/GpsMathTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Numerics;
+using com.csutil.gps;
+using Xunit;
+
+namespace com.csutil.tests.gps {
+
+    [Collection("Sequential")] // Will execute tests in here sequentially
+    public class GpsMathTests {
+
+        public GpsMathTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public void ExampleUsage1() {
+
+            // https://goo.gl/maps/znXp1bvnJcKuDuiW8
+            var gps1 = new GpsPoint(33.742120845642226, -112.63922018861862);
+            var gps2 = new GpsPoint(33.75042155450956, -112.63346603909699);
+            var gps3 = new GpsPoint(33.74212852767701, -112.62770384672456);
+
+            var zeroPoint = gps1; // Use the gps 1 point as the reference for all local math
+
+            var p1 = gps1.ToRelativeCoordsInMeters(zeroPoint);
+            var p2 = gps2.ToRelativeCoordsInMeters(zeroPoint);
+            var p3 = gps3.ToRelativeCoordsInMeters(zeroPoint);
+
+            // Since the GPS1 point was used as the reference zero point, the rel. distance must be 0:
+            Assert.Equal(Vector3.Zero, p1);
+
+            // On the relative points any normal vector math can be done, eg calculating distances:
+            {
+                var fromP1ToP2 = p2 - p1;
+                int distanceInMeters = (int)fromP1ToP2.Length();
+                Assert.Equal(1065, distanceInMeters);
+            }
+            {
+                var fromP1ToP3 = p3 - p1;
+                int distanceInMeters = (int)fromP1ToP3.Length();
+                Assert.Equal(1066, distanceInMeters);
+                // GPS3 is exactly east of GPS1 so the distance on the x axis is also 1066:
+                Assert.Equal(1066, (int)fromP1ToP3.X);
+                // And the distance is nearly 0 meters on the north axis:
+                Assert.True(Math.Abs(fromP1ToP3.Z) < 1);
+            }
+
+            // Converting relative points back to GPS coordinates:
+            var gps1_2 = GpsPoint.RelativePointToGpsCoords(zeroPoint, p1);
+            Assert.Equal(gps1.Latitude, gps1_2.Latitude, precision: 8);
+            Assert.Equal(gps1.Longitude, gps1_2.Longitude, precision: 8);
+
+            var gps2_2 = GpsPoint.RelativePointToGpsCoords(zeroPoint, p2);
+            Assert.Equal(gps2.Latitude, gps2_2.Latitude, precision: 8);
+            Assert.Equal(gps2.Longitude, gps2_2.Longitude, precision: 8);
+
+            var gps3_2 = GpsPoint.RelativePointToGpsCoords(zeroPoint, p3);
+            Assert.Equal(gps3.Latitude, gps3_2.Latitude, precision: 8);
+            Assert.Equal(gps3.Longitude, gps3_2.Longitude, precision: 8);
+
+        }
+
+        private class GpsPoint : IHasLatLong {
+
+            public double Latitude { get; }
+            public double Longitude { get; }
+
+            private readonly double[] _relativeCoordinates = new double[2];
+
+            public GpsPoint(double latitude, double longitude) {
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+
+            public Vector3 ToRelativeCoordsInMeters(IHasLatLong zeroPoint) {
+                this.CalcRelativeCoordsInMeters(_relativeCoordinates, zeroPoint);
+                var altitudeInMeters = 0;
+                return new Vector3((float)_relativeCoordinates[0], altitudeInMeters, (float)_relativeCoordinates[1]);
+            }
+
+            public static GpsPoint RelativePointToGpsCoords(GpsPoint zeroPoint, Vector3 relativeCoordinates) {
+                double[] result = new double[2];
+                zeroPoint.CalcGpsCoords(relativeCoordinates.X, relativeCoordinates.Z, result);
+                return new GpsPoint(result[0], result[1]);
+            }
+
+        }
+
+    }
+
+}
