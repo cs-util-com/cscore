255d412e579b65bde60e4d2069a23a54d66cbb54
S <code@csutil.com>
2022-10-15 08:31:48 +0200

    Some refactoring to move the xml parsing logic to its own class

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/XmlParser.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace com.csutil.xml {
+
+    public static class XmlParser {
+
+        public static T ParseAsXmlInto<T>(this Stream self) {
+            using (var r = XmlReader.Create(self)) {
+                return (T)new XmlSerializer(typeof(T)).Deserialize(r);
+            }
+        }
+
+        public static T ParseAsXmlInto<T>(this TextReader self) {
+            using (var r = XmlReader.Create(self)) {
+                return (T)new XmlSerializer(typeof(T)).Deserialize(r);
+            }
+        }
+
+        public static T ParseAsXmlInto<T>(string xmlString) {
+            using (var reader = new StringReader(xmlString)) {
+                return reader.ParseAsXmlInto<T>();
+            }
+        }
+
+    }
+
+}
