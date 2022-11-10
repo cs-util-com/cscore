24a9b8268aa3e14f298aac1b430dd9e360c70068
S <code@csutil.com>
2022-11-10 07:51:43 +0100

    Improved mesh triangulation to ensure that the global pose of the triangulator does not affect the created mesh

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/extensions/MeshFilterExtensions.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/extensions/MeshFilterExtensions.cs
@@ -10,9 +10,16 @@ namespace com.csutil {
         }
 
         public static Mesh TriangulateMesh(this PolygonCollider2D triangulator, Vector2[] polygonPoints) {
+            var oldPose = triangulator.transform.CopyPose();
+            // Set global pose to identity so that the triangulation is not affected by the local pose:
+            triangulator.transform.position = Vector3.zero;
+            triangulator.transform.rotation = Quaternion.identity;
+            triangulator.transform.scale(Vector3.one);
             triangulator.points = polygonPoints;
             triangulator.useDelaunayMesh = true;
-            return triangulator.CreateMesh(false, false);
+            var result = triangulator.CreateMesh(false, false);
+            triangulator.transform.ApplyPose(oldPose);
+            return result;
         }
 
     }
