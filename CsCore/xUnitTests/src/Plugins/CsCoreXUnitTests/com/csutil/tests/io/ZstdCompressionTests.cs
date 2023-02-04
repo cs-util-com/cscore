7c1b0598e09ab19670c1847e2caebfa72d22c114
S <code@csutil.com>
2023-02-04 15:43:39 +0100

    Added C# version of Zstandard library ( https://www.nuget.org/packages/ZstdSharp.Port )

---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/io/ZstdCompressionTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Xunit;
+using ZstdSharp;
+
+namespace com.csutil.tests {
+
+    public class ZstdCompressionTests {
+
+        public ZstdCompressionTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public void ExampleUsage1() {
+
+            var dir = EnvironmentV2.instance.GetOrAddTempFolder("ZstdCompressionTests");
+            var inputFile = dir.GetChild("SomeFile1.txt");
+            var zippedFile = dir.GetChild("SomeFile1.txt.zst");
+            inputFile.WriteAllText("Some text to compress");
+
+            { // Streaming compression:
+                using var inputStream = inputFile.OpenForRead();
+                using var targetStreamToWriteTo = zippedFile.OpenOrCreateForWrite();
+                using var compressionStream = new CompressionStream(targetStreamToWriteTo, Compressor.MaxCompressionLevel);
+                inputStream.CopyTo(compressionStream);
+            }
+            { // Streaming decompression:
+                using var input = zippedFile.OpenForRead();
+                using var output = new MemoryStream();
+                using var decompressionStream = new DecompressionStream(input);
+                decompressionStream.CopyTo(output);
+                output.ResetStreamCurserPositionToBeginning();
+                var decompressedText = new StreamReader(output).ReadToEnd();
+                Assert.Equal(inputFile.ReadAllText(), decompressedText);
+            }
+        }
+
+    }
+
+}
