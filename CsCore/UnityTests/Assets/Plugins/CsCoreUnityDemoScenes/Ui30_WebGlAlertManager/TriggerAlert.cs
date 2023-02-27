c26076280fc82646505e95fd91a13d3b38953165
David Brenn <davidbrenn@t-online.de>
2023-02-27 11:48:49 +0100

    Add AlertManager DemoScene

---
+++ CsCore/UnityTests/Assets/Plugins/CsCoreUnityDemoScenes/Ui30_WebGlAlertManager/TriggerAlert.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using com.csutil.webgl.alert;
+
+/// <summary>
+/// Gets the textField input and sends it with the AlertManager
+/// </summary>
+public class TriggerAlert : MonoBehaviour
+{
+    public GameObject textField;
+    public GameObject alertManager;
+
+
+    public void alertBrowserFromTextField()
+    {
+        alertManager.GetComponent<AlertManager>().sendAlert(textField.GetComponent<InputField>().text);
+    }
+
+  
+}
