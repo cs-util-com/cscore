0f8647403bb686c42cd874fdd830ac94ce31b716
S <code@csutil.com>
2023-03-17 06:42:08 +0100

    Unpacked the demo and some code formatting cleanups

---
+++ CsCore/CsCoreUnityWebGL/MoreDemos/CsCoreUnityDemoScenesWebGL/WebGL1_AlertManager/TriggerAlert.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using com.csutil.webgl.alert;
+
+namespace com.csutil.tests.ui {
+
+    /// <summary> Gets the textField input and sends it with the AlertManager </summary>
+    public class TriggerAlert : MonoBehaviour {
+    
+        public GameObject textField;
+        public GameObject alertManager;
+
+        public void alertBrowserFromTextField() {
+            alertManager.GetComponent<AlertManager>().triggerBrowserAlert(textField.GetComponent<InputField>().text);
+        }
+
+    }
+    
+}
