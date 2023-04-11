1cb7631d220f70665945927a3f55c7db6696612b
S <code@csutil.com>
2023-04-11 21:49:45 +0200

    Added usage example of ChatGPT v4, using the same prompt to compare quality of v3 to v4 when telling jokes (v4 actually produces a few good ones where v3 never really created responses that were believable)

--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/http/apis/OpenAiTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/http/apis/OpenAiTests.cs
@@ -51,6 +51,26 @@ namespace com.csutil.tests.http {
             Log.d("response.content=" + JsonWriter.AsPrettyString(messages));
         }
 
+        [Fact]
+        public async Task ExampleUsage4_ChatGpt4() {
+            var openAi = new OpenAi(await IoC.inject.GetAppSecrets().GetSecret("OpenAiKey"));
+            var messages = new List<ChatGpt.Line>() {
+                new ChatGpt.Line(ChatGpt.Role.system, content: "You are a standup comedian. You are on stage and about to tell a joke."),
+                new ChatGpt.Line(ChatGpt.Role.user, content: "Do you know the joke about the chicken that crossed the road?"),
+                new ChatGpt.Line(ChatGpt.Role.assistant, content: "Yes I actually happen to know the best one of all chicken jokes."),
+                new ChatGpt.Line(ChatGpt.Role.user, content: "Why did the chicken cross the road?"),
+            };
+            var request = new ChatGpt.Request(messages);
+            request.model = "gpt-4"; // See https://platform.openai.com/docs/models/gpt-4
+            var response = await openAi.ChatGpt(request);
+            ChatGpt.Line newLine = response.choices.Single().message;
+            Assert.Equal("" + ChatGpt.Role.assistant, newLine.role);
+            Assert.NotEmpty(newLine.content);
+
+            messages.Add(newLine);
+            Log.d("response.content=" + JsonWriter.AsPrettyString(messages));
+        }
+        
     }
 
 }
