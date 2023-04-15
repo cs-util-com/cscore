1eec89ff1b85f5735089ed24c962dcfbdb06919c
S <code@csutil.com>
2023-04-15 05:51:12 +0200

    Added 2 new valuable additions to the default packages

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/editor/CsUtilUnityEditorMenu.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/editor/CsUtilUnityEditorMenu.cs
@@ -37,6 +37,12 @@ namespace com.csutil.editor {
             // https://docs.unity3d.com/Packages/com.unity.mobile.android-logcat@1.3
             UnityPackageInstaller.AddToProjectViaPackageManager("com.unity.mobile.android-logcat", "1.3.2");
 
+            // Efficient code recompiling via https://github.com/handzlikchris/FastScriptReload
+            UnityPackageInstaller.AddToProjectViaPackageManager("com.fastscriptreload", "https://github.com/handzlikchris/FastScriptReload.git?path=Assets");
+            
+            // FPS monitoring via https://github.com/Tayx94/graphy
+            UnityPackageInstaller.AddToProjectViaPackageManager("com.tayx.graphy", "https://github.com/Tayx94/graphy.git");
+            
             // Seems to cause build errors for Android, so not stable enough yet?
             // https://docs.unity3d.com/Packages/com.unity.build-report-inspector@0.3
             // UnityPackageInstaller.AddToProjectViaPackageManager("com.unity.build-report-inspector", "0.3.0-preview");
