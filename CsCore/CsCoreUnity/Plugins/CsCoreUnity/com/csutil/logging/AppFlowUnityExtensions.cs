a170fa5d4acf8b764ec9fe77619efb08c8d3173e
S <code@csutil.com>
2023-04-19 14:59:58 +0200

    Bugfix for new text input dialog

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/logging/AppFlowUnityExtensions.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/logging/AppFlowUnityExtensions.cs
@@ -68,8 +68,8 @@ namespace com.csutil {
                 var action = UiEvents.CONFIRM_CANCEL_DIALOG + "_" + d.caption + (d.dialogWasConfirmed ? " CONFIRMED" : " CANCELED");
                 self.TrackEvent(EventConsts.catUi, action, d);
             });
-            EventBus.instance.Subscribe(self, EventConsts.catUi + UiEvents.INPUT_DIALOG, (ConfirmCancelDialog d) => {
-                var action = UiEvents.INPUT_DIALOG + "_" + d.caption + (d.dialogWasConfirmed ? " CONFIRMED" : " CANCELED");
+            EventBus.instance.Subscribe(self, EventConsts.catUi + UiEvents.INPUT_DIALOG, (DialogForTextInput d) => {
+                var action = UiEvents.INPUT_DIALOG + "_" + d.caption;
                 self.TrackEvent(EventConsts.catUi, action, d);
             });
         }
