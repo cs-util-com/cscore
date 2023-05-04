f9c77f2c670c98708dc586e6b0f56389338f0a91
S <code@csutil.com>
2023-05-04 09:48:09 +0200

    Added an ECS serialization system that supports templates/prefabs and variants using JSON diffs

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/ecs/EntityComponentSystem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Threading.Tasks;
+using com.csutil.json;
+using JsonDiffPatchDotNet;
+using Newtonsoft.Json;
+using Zio;
+using Newtonsoft.Json.Linq;
+
+namespace com.csutil.model.ecs {
+
+    public class EntityComponentSystem<T> where T : IEntityData {
+
+        private readonly DirectoryEntry EntityDir;
+        private readonly JsonDiffPatch JonDiffPatch = new JsonDiffPatch();
+        private readonly Dictionary<string, JToken> JTokens = new Dictionary<string, JToken>();
+
+        private Func<JsonSerializer> GetJsonSerializer = () => JsonSerializer.Create(JsonNetSettings.typedJsonSettings);
+
+        public EntityComponentSystem(DirectoryEntry entityDir) {
+            this.EntityDir = entityDir;
+        }
+
+        public async Task LoadAllJTokens() {
+            var jsonSerializer = GetJsonSerializer();
+            var tasks = new List<Task>();
+            foreach (var templateFile in EntityDir.EnumerateFiles()) {
+                tasks.Add(TaskV2.Run((() => LoadJTokenFromFile(templateFile, jsonSerializer))));
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        private void LoadJTokenFromFile(FileEntry templateFile, JsonSerializer jsonSerializer) {
+            using (var stream = templateFile.OpenForRead()) {
+                JToken templateJToken = jsonSerializer.Deserialize<JToken>(new JsonTextReader(new StreamReader(stream)));
+                UpdateJTokens(templateFile.Name, templateJToken);
+            }
+        }
+
+        private void UpdateJTokens(string id, JToken jtoken) {
+            JTokens[id] = jtoken;
+        }
+
+        public void Save(T instance) {
+            var entityId = instance.GetId();
+            entityId.ThrowErrorIfNullOrEmpty("entity.Id");
+            var file = EntityDir.GetChild(entityId);
+            var json = ToJToken(instance, GetJsonSerializer());
+            var templateId = instance.TemplateId;
+            if (templateId != null) {
+                var template = JTokens[templateId];
+                json = JonDiffPatch.Diff(template, json);
+            }
+            UpdateJTokens(entityId, json);
+            file.SaveAsJson(json);
+        }
+
+        private JToken ToJToken(T instance, JsonSerializer serializer) { return JToken.FromObject(instance, serializer); }
+
+        public T CreateVariantOf(T entity) {
+            if (!JTokens.ContainsKey(entity.GetId())) { throw new KeyNotFoundException("Template not found: " + entity.GetId()); }
+            JsonSerializer serializer = GetJsonSerializer();
+            var json = ToJToken(entity, serializer);
+            json["Id"] = "" + GuidV2.NewGuid();
+            json["TemplateId"] = entity.GetId();
+            return json.ToObject<T>(serializer);
+        }
+
+        public IEnumerable<string> GetAllEntityIds() {
+            return JTokens.Keys;
+        }
+
+        public async Task<T> Load(string entityId) {
+            return ComposeFullJToken(entityId).ToObject<T>(GetJsonSerializer());
+        }
+
+        /// <summary> Recursively composes the full json for the given entity id by applying the templates </summary>
+        private JToken ComposeFullJToken(string entityId) {
+            if (!JTokens.ContainsKey(entityId)) { throw new KeyNotFoundException("Entity not found: " + entityId); }
+            var json = JTokens[entityId];
+            if (json["TemplateId"] is JArray templateIdArray) {
+                var templateId = templateIdArray[1].Value<string>();
+                var template = ComposeFullJToken(templateId);
+                json = JonDiffPatch.Patch(template, json);
+            }
+            return json;
+        }
+
+    }
+
+    public interface IEntityData : HasId {
+
+        string Id { get; }
+        string TemplateId { get; }
+        Matrix4x4? LocalPose { get; }
+        IList<IComponentData> Components { get; }
+        IList<string> ChildrenIds { get; }
+        IList<string> Tags { get; }
+
+    }
+
+    public interface IComponentData : HasId {
+
+    }
+
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/model/esc/EntityComponentSystemTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using com.csutil.json;
+using com.csutil.model.ecs;
+using Xunit;
+using Zio;
+
+namespace com.csutil.tests.model.esc {
+
+    public class EntityComponentSystemTests {
+
+        public EntityComponentSystemTests(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public async Task ExampleUsage1() {
+
+            var rootDir = EnvironmentV2.instance.GetOrAddTempFolder("EntityComponentSystemTests_ExampleUsage1");
+            var entitiesDir = rootDir.GetChildDir("Entities");
+            entitiesDir.DeleteV2();
+            entitiesDir.CreateV2();
+
+            var ecs = new EntityComponentSystem<Entity>(entitiesDir);
+
+            var enemyTemplate = new Entity() {
+                Id = "" + GuidV2.NewGuid(),
+                LocalPose = Matrix4x4.CreateTranslation(1, 2, 3),
+                Components = new List<IComponentData>() {
+                    new EnemyComp() { Id = "c1", Health = 100 }
+                }
+            };
+
+            ecs.Save(enemyTemplate);
+
+            // An instance that has a different health value than the template:
+            Entity variant1 = ecs.CreateVariantOf(enemyTemplate);
+            (variant1.Components.Single() as EnemyComp).Health = 200;
+            ecs.Save(variant1);
+
+            // Create a variant2 of the variant1
+            Entity variant2 = ecs.CreateVariantOf(variant1);
+            (variant2.Components.Single() as EnemyComp).Health = 300;
+            ecs.Save(variant2);
+
+            // Another instance that is identical to the template:
+            Entity variant3 = ecs.CreateVariantOf(enemyTemplate);
+            ecs.Save(variant3);
+
+
+            var ecs2 = new EntityComponentSystem<Entity>(entitiesDir);
+            await ecs2.LoadAllJTokens();
+            var ids = ecs2.GetAllEntityIds().ToList();
+            Assert.Equal(4, ids.Count());
+            Entity v1 = await ecs2.Load(variant1.Id);
+            Assert.Equal(200, (v1.Components.Single() as EnemyComp).Health);
+
+            Entity v2 = await ecs2.Load(variant2.Id);
+            Assert.Equal(300, (v2.Components.Single() as EnemyComp).Health);
+
+        }
+
+        private class Entity : IEntityData {
+            public string Id { get; set; }
+            public string TemplateId { get; }
+            public Matrix4x4? LocalPose { get; set; }
+            public IList<IComponentData> Components { get; set; }
+            public IList<string> ChildrenIds { get; set; }
+            public IList<string> Tags { get; set; }
+
+            public string GetId() { return Id; }
+        }
+
+        private class EnemyComp : IComponentData {
+            public string Id { get; set; }
+            public int Health;
+            public string GetId() { return Id; }
+        }
+
+    }
+
+}
