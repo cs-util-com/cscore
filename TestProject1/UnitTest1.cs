ebbb43beea8fb551b8ea78ac13641ae53baf4e85
Radu-Mihai Castravet <devicedev@protonmail.com>
2023-11-28 17:41:01 +0100

    refactor(UnitTest1): Refactor code for the tts request

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,95 +1,53 @@
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using Xunit;
-using com.csutil;
-using com.csutil.http.apis;
-using System.Diagnostics;
-using System.IO;
-using System.Text.Json;
-using System.Net;
 using System.Text;
+using Newtonsoft.Json;
 namespace SPPTest;
 
 public class UnitTest1
 {
-    string openAiKey = File.ReadAllText(@"C:\Users\nicol\Desktop\VSCSem3\cscore\TestProject1\env.txt");
 
-    // [Fact]
-    // public async Task ExampleUsage3_ChatGpt()
-    // {
+    static string openAISpeechURL = "https://api.openai.com/v1/audio/speech";
 
-
-    //     var openAi = new OpenAi(openAiKey);
-    //     var messages = new List<ChatGpt.Line>() {
-    //             new ChatGpt.Line(ChatGpt.Role.system, content: "You are a standup comedian. You are on stage and about to tell a joke."),
-    //             new ChatGpt.Line(ChatGpt.Role.user, content: "Do you know the joke about the chicken that crossed the road?"),
-    //             new ChatGpt.Line(ChatGpt.Role.assistant, content: "Yes I actually happen to know the best one of all chicken jokes."),
-    //             new ChatGpt.Line(ChatGpt.Role.user, content: "Why did the chicken cross the road?"),
-    //         };
-    //     var response = await openAi.ChatGpt(new ChatGpt.Request(messages));
-    //     ChatGpt.Line newLine = response.choices.Single().message;
-    //     Assert.Equal("" + ChatGpt.Role.assistant, newLine.role);
-    //     Assert.NotEmpty(newLine.content);
-
-    //     messages.Add(newLine);
-    //     Log.d("response.content=" + JsonWriter.AsPrettyString(messages));
-    // }
+    static string currentDirectory = Directory.GetCurrentDirectory();
+    static string audioFolderPath = Path.GetFullPath(Path.Combine(currentDirectory, @"../../../audio/"));
+    static string openAiKey = File.ReadAllText(Path.GetFullPath(Path.Combine(currentDirectory, @"../../../env.txt")));
 
     [Fact]
     public async Task ExampleTTS()
     {
+        string input = "Hello World";
 
-        string apiUrl = "https://api.openai.com/v1/audio/speech";
-        string token = openAiKey;
-
-        // Create HttpClient
         using (HttpClient client = new HttpClient())
         {
-            // Set the authorization header with the Bearer token
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            SetBearerToken(client, openAiKey);
 
-            // Define the JSON payload
-            string jsonPayload = "{\"model\": \"tts-1\", \"voice\": \"alloy\",\"input\": \"please work\"}";
+            HttpResponseMessage response = await MakeTTSRequest(client, input);
+            Assert.True(response.IsSuccessStatusCode);
 
-            // Create the content for the request
-            StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            byte[] responseContent = await response.Content.ReadAsByteArrayAsync();
+            Assert.True(responseContent.Length > 0);
 
-            // Send the POST request
-            HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+            string outputPath = audioFolderPath + "speech.mp3";
 
-            // Check the response status
-            if (response.IsSuccessStatusCode)
-            {
-                // Read and display the response content
-                byte[] responseContent = await response.Content.ReadAsByteArrayAsync();
-                string outputPath = @"C:\Users\nicol\Desktop\VSCSem3\cscore\TestProject1\response.mp3";
-                File.WriteAllBytes(outputPath, responseContent);
-                Console.WriteLine($"Success! Response: {responseContent}");
+            File.WriteAllBytes(outputPath, responseContent);
+            Assert.True(File.Exists(outputPath));
 
-            }
-            else
-            {
-                Console.WriteLine($"Error! Status Code: {response.StatusCode}");
-            }
+            Console.WriteLine("Saved audio to " + outputPath);
+
         }
-
-
-
-
-
-        // call speech with await
-        //var response = await Speech("Hello World");
     }
-    public Task<HttpWebResponse> Speech(string input)
+    void SetBearerToken(HttpClient client, string token)
     {
-        return CreateSpeechRequest(new MyTTSRequest(input));
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
     }
-    public Task<HttpWebResponse> CreateSpeechRequest(MyTTSRequest requestObject)
+    async Task<HttpResponseMessage> MakeTTSRequest(HttpClient client, string input)
     {
-        var request = new Uri("https://api.openai.com/v1/audio/speech").SendPOST();
-        var aa = request.WithAuthorization(openAiKey).WithJsonContent(requestObject);
-        return aa.GetResult<HttpWebResponse>();
+        MyTTSRequest ttsRequest = new MyTTSRequest(input);
+        string jsonPayload = JsonConvert.SerializeObject(ttsRequest);
+        StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await client.PostAsync(openAISpeechURL, content);
+        return response;
     }
-    // create a class with fields model, input and voice
+
     public class MyTTSRequest
     {
         public string model { get; set; } = "tts-1";
