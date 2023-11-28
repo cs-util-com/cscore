0e669982ac4e5db7bb3c3e0c541e136ee6768681
Radu-Mihai Castravet <devicedev@protonmail.com>
2023-11-28 16:27:59 +0100

    wip: working on creating request/response for tts endpoint

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -3,28 +3,60 @@ using Xunit;
 using com.csutil;
 using com.csutil.http.apis;
 using System.Diagnostics;
-
+using System.IO;
+using System.Text.Json;
 namespace SPPTest;
 
 public class UnitTest1
 {
-    [Fact]
-    public async Task ExampleUsage3_ChatGpt()
-    {
-        var key = "";
-        var openAi = new OpenAi(key);
-        var messages = new List<ChatGpt.Line>() {
-                new ChatGpt.Line(ChatGpt.Role.system, content: "You are a standup comedian. You are on stage and about to tell a joke."),
-                new ChatGpt.Line(ChatGpt.Role.user, content: "Do you know the joke about the chicken that crossed the road?"),
-                new ChatGpt.Line(ChatGpt.Role.assistant, content: "Yes I actually happen to know the best one of all chicken jokes."),
-                new ChatGpt.Line(ChatGpt.Role.user, content: "Why did the chicken cross the road?"),
-            };
-        var response = await openAi.ChatGpt(new ChatGpt.Request(messages));
-        ChatGpt.Line newLine = response.choices.Single().message;
-        Assert.Equal("" + ChatGpt.Role.assistant, newLine.role);
-        Assert.NotEmpty(newLine.content);
+    string openAiKey = File.ReadAllText("/Users/devicedev/Documents/RWTH/5 Semester/Holobuilder Lab/cscore/TestProject1/env.txt");
 
-        messages.Add(newLine);
-        Log.d("response.content=" + JsonWriter.AsPrettyString(messages));
+    // [Fact]
+    // public async Task ExampleUsage3_ChatGpt()
+    // {
+
+
+    //     var openAi = new OpenAi(openAiKey);
+    //     var messages = new List<ChatGpt.Line>() {
+    //             new ChatGpt.Line(ChatGpt.Role.system, content: "You are a standup comedian. You are on stage and about to tell a joke."),
+    //             new ChatGpt.Line(ChatGpt.Role.user, content: "Do you know the joke about the chicken that crossed the road?"),
+    //             new ChatGpt.Line(ChatGpt.Role.assistant, content: "Yes I actually happen to know the best one of all chicken jokes."),
+    //             new ChatGpt.Line(ChatGpt.Role.user, content: "Why did the chicken cross the road?"),
+    //         };
+    //     var response = await openAi.ChatGpt(new ChatGpt.Request(messages));
+    //     ChatGpt.Line newLine = response.choices.Single().message;
+    //     Assert.Equal("" + ChatGpt.Role.assistant, newLine.role);
+    //     Assert.NotEmpty(newLine.content);
+
+    //     messages.Add(newLine);
+    //     Log.d("response.content=" + JsonWriter.AsPrettyString(messages));
+    // }
+
+    [Fact]
+    public async Task ExampleTTS()
+    {
+        // call speech with await
+        var response = await Speech("Hello World");
     }
+    public Task<OpenAi.Text.CompletionsResponse> Speech(string input)
+    {
+        return CreateSpeechRequest(new MyTTSRequest(input));
+    }
+    public Task<OpenAi.Text.CompletionsResponse> CreateSpeechRequest(MyTTSRequest requestObject)
+    {
+        var request = new Uri("https://api.openai.com/v1/audio/speech").SendPOST();
+        return request.WithAuthorization(openAiKey).WithJsonContent(requestObject).GetResult<OpenAi.Text.CompletionsResponse>();
+    }
+    // create a class with fields model, input and voice
+    public class MyTTSRequest
+    {
+        public string model { get; set; } = "tts-1";
+        public string input { get; set; }
+        public string voice { get; set; } = "alloy";
+        public MyTTSRequest(string input)
+        {
+            this.input = input;
+        }
+    }
+
 }
