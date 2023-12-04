bdf4640cc66c7cc7e81847c47b4af40416587485
S <code@csutil.com>
2023-12-04 06:09:08 +0100

    Now the box collider can be factorX smaller or larger then the sprite when the sprite is resized

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/extensions/ImageExtensions.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/extensions/ImageExtensions.cs
@@ -122,13 +122,13 @@ namespace com.csutil {
             finally { RenderTexture.ReleaseTemporary(tempRenderTex); }
         }
         
-        public static void SetSpriteRendererWidthToFitTextureAspectRatio(this SpriteRenderer self) {
+        public static void SetSpriteRendererWidthToFitTextureAspectRatio(this SpriteRenderer self, float boxColliderShrinkFactor = 1f) {
             var sprite = self.sprite;
             var aspectRatio = sprite.textureRect.width / sprite.textureRect.height;
             var correctedSizeX = aspectRatio * self.size.y;
             self.size = new Vector2(correctedSizeX, self.size.y); 
             var boxCollider = self.GetComponent<BoxCollider>();
-            if (boxCollider != null) { boxCollider.size = self.size; }
+            if (boxCollider != null) { boxCollider.size = self.size * boxColliderShrinkFactor; }
         }
         
     }
