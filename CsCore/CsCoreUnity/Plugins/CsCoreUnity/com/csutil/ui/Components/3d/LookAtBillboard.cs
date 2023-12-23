f5c2d7a23417a232395995533f827bfc902426fb
S <code@csutil.com>
2023-12-23 07:25:52 +0100

    Improved the LookAt Billboard to by default not use the forward approach and instead a look at target approach so that when the user rotates the camera the object does not rotate, looks more natural in most cases

--- CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/3d/LookAtBillboard.cs
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/ui/Components/3d/LookAtBillboard.cs
@@ -10,21 +10,35 @@ namespace com.csutil {
         public float lerpOmega = 20;
         private Vector3 _lerpVelocity = Vector3.zero;
         public bool onlyRotateAroundY = false;
+        public bool useTransformForwardAsTarget = false;
 
         private void OnEnable() {
             if (targetToLookAt == null) { targetToLookAt = Camera.main.transform; }
         }
 
         private void Update() {
-            var newForward = targetToLookAt.forward;
-            if (onlyRotateAroundY) {
-                newForward = Vector3.Scale(newForward, new Vector3(1, 0, 1)).normalized;
-            }
-            if (lerpSpeed > 0) {
-                transform.forward = transform.forward.LerpWithVelocity(newForward, ref _lerpVelocity, Time.deltaTime * lerpSpeed, lerpOmega);
+            if (useTransformForwardAsTarget) {
+                var newForward = targetToLookAt.forward;
+                if (onlyRotateAroundY) {
+                    newForward = Vector3.Scale(newForward, new Vector3(1, 0, 1)).normalized;
+                }
+                if (lerpSpeed > 0) {
+                    transform.forward = transform.forward.LerpWithVelocity(newForward, ref _lerpVelocity, Time.deltaTime * lerpSpeed, lerpOmega);
+                } else {
+                    transform.forward = newForward;
+                }
             } else {
-                transform.forward = newForward;
+                if (lerpSpeed > 0) {
+                    // Lerp the rotation of the object to face the target rotation
+                    var vector = transform.position - targetToLookAt.position;
+                    if (onlyRotateAroundY) { vector.y = 0; }
+                    var targetRotation = Quaternion.LookRotation(vector);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * lerpSpeed);
+                } else {
+                    transform.LookAt(targetToLookAt);
+                }
             }
+
         }
 
     }
