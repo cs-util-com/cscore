a06bb3cc6cc51f7fef2334177fc724d52e6cc5c2
Jeroen Haack <Ron.Haack@web.de>
2024-01-02 14:17:19 +0100

    Added ImageBlur and non function testing for ImageBlur

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/images/ImageBlur.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ImageBlur {
+
+    // Helper method to set the color at a specific location in the image array
+    private static void SetColorAt(byte[] imageData, int x, int y, int width, byte[] color, int bytesPerPixel) {
+        int startIdx = (y * width + x) * bytesPerPixel;
+        Array.Copy(color, 0, imageData, startIdx, color.Length);
+    }
+
+    // Helper method to get color at a given position
+    private static byte[] GetColorAt(byte[] img, int x, int y, int bytesPerPixel, int width) {
+        int startIdx = (y * width + x) * bytesPerPixel;
+        return new byte[] { img[startIdx], img[startIdx + 1], img[startIdx + 2] };
+    }
+
+    //Potentially optimizable by skipping pixels already in for loop instead of checking throgh if statement 
+
+    public static byte[] RunBoxBlur(byte[] inputImage, int width, int height, int radius, int bytePerPixel) {
+        byte[] outputImage = new byte[inputImage.Length];
+        int stride = width * bytePerPixel;
+        for (int y = radius; y < height - radius; y++) {
+            for (int x = radius; x < width - radius; x++) {
+                int[] sumColor = new int[bytePerPixel];
+                int count = 0;
+
+                for (int dy = -radius; dy <= radius; dy++) {
+                    for (int dx = -radius; dx <= radius; dx++) {
+                        int positionx = x + dx;
+                        int positiony = y + dy;
+
+                        if (positionx >= 0 && positionx < width && positiony >= 0 && positiony < height) {
+                            byte[] color = new byte[bytePerPixel];
+                            Array.Copy(inputImage, x, color, 0, bytePerPixel);
+                            for (int i = 0; i < bytePerPixel; i++) {
+                                sumColor[i] += color[i];
+                            }
+                            count++;
+                        }
+                    }
+                }
+                byte[] avgColor = new byte[bytePerPixel];
+                for (int i = 0; i < bytePerPixel; i++) {
+                    avgColor[i] = (byte)(sumColor[i] / count);
+                }
+
+                SetColorAt(outputImage, x, y, width, avgColor, bytePerPixel);
+
+            }
+        }
+        return outputImage;
+    }
+}
+   
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageBlurTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using com.csutil.algorithms.images;
+using com.csutil.io;
+using com.csutil.model;
+using StbImageSharp;
+using Xunit;
+using Zio;
+
+
+namespace com.csutil.tests.AlgorithmTests {
+    public class ImageBlurTest {
+        public ImageBlurTest(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public async Task RunBoxBlur_ShouldApplyBlurCorrectly() {
+
+            var folder = EnvironmentV2.instance.GetOrAddAppDataFolder("BlurTesting");
+
+            var imageFile = folder.GetChild("GT04-image.png");
+            await DownloadFileIfNeeded(imageFile, "http://atilimcetin.com/global-matting/GT04-image.png");
+
+            ImageResult image = await ImageLoader.LoadImageInBackground(imageFile);
+            byte[] imageResult = ImageBlur.RunBoxBlur(image.Data, image.Width, image.Height, 1, (int)image.ColorComponents);
+            ImageResult result = ImageResult.FromMemory(imageResult);
+
+        }
+        private static async Task DownloadFileIfNeeded(FileEntry self, string url) {
+            var imgFileRef = new MyFileRef() { url = url, fileName = self.Name };
+            await imgFileRef.DownloadTo(self.Parent, useAutoCachedFileRef: true);
+        }
+        private class MyFileRef : IFileRef {
+            public string dir { get; set; }
+            public string fileName { get; set; }
+            public string url { get; set; }
+            public Dictionary<string, object> checksums { get; set; }
+            public string mimeType { get; set; }
+        }
+    }        
+
+}
