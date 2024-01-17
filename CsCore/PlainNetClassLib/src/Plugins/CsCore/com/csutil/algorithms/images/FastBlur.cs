b088f223ea221f2fc7a530d30775f0ded53f72d8
Jeroen Haack <Ron.Haack@web.de>
2024-01-17 04:25:37 +0100

    Implemented fastblur which runs at 760ms while boxblur runs at 20,3 secs on my machine. Still has issues with artifacts and slightly dragged edges.

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/images/FastBlur.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Xml.Schema;
+
+namespace com.csutil.algorithms.images
+{
+    public class FastBlur
+    {
+        private static void SetColorAt(byte[] imageData, int x, int y, int width, byte[] color, int bytesPerPixel)
+        {
+            int startIdx = (y * width + x) * bytesPerPixel;
+            Array.Copy(color, 0, imageData, startIdx, color.Length);
+        }
+
+
+
+        // Helper method to get color at a given position
+        private static byte[] GetColorAt(byte[] img, int x, int y, int bytesPerPixel, int width)
+        {
+            int startIdx = (y * width + x) * bytesPerPixel;
+            return new byte[] { img[startIdx], img[startIdx + 1], img[startIdx + 2], img[startIdx + 3] };
+        }
+
+        public static byte[] FastBoxBlur(byte[] img, int width, int height, int radius, int channels)
+        {
+            var kSize = radius;
+
+            if (kSize % 2 == 0) kSize++;
+            var hBlur = new byte[img.Length];
+            Array.Copy(img, hBlur, img.Length);
+            for(int j = 0; j < height; j++)
+            {
+                var hSum = new int[] { 0, 0, 0, 0, };
+                var iAvg = new int[] { 0, 0, 0, 0, };
+                for(int x = 0; x < kSize; x++)
+                {
+                    var tmpColor = GetColorAt(hBlur, x, j, 4, width);
+                    hSum[0] += tmpColor[0];
+                    hSum[1] += tmpColor[1];
+                    hSum[2] += tmpColor[2];
+                    hSum[3] += tmpColor[3];
+                }
+                iAvg[0] = hSum[0] / kSize;
+                iAvg[1] = hSum[1] / kSize;
+                iAvg[2] = hSum[2] / kSize;
+                iAvg[3] = hSum[3] / kSize;
+                for(int i = 0; i < width; i++)
+                {
+                    if(i - kSize/2 >= 0 && i + 1 + kSize/2 < width)
+                    {
+                        var tmp_pColor = GetColorAt(hBlur, i - kSize / 2, j, 4, width);
+                        hSum[0] -= tmp_pColor[0];
+                        hSum[1] -= tmp_pColor[1];
+                        hSum[2] -= tmp_pColor[2];
+                        hSum[3] -= tmp_pColor[3];
+                        var tmp_nColor = GetColorAt(hBlur, i + 1 + kSize / 2, j, 4, width);
+                        hSum[0] += tmp_nColor[0];
+                        hSum[1] += tmp_nColor[1];
+                        hSum[2] += tmp_nColor[2];
+                        hSum[3] += tmp_nColor[3];
+                        //
+                        iAvg[0] = hSum[0] / kSize;
+                        iAvg[1] = hSum[1] / kSize;
+                        iAvg[2] = hSum[2] / kSize;
+                        iAvg[3] = hSum[3] / kSize;
+                    }
+                    var bAvg = new byte[iAvg.Length];
+                    bAvg[0] = (byte)iAvg[0];
+                    bAvg[1] = (byte)iAvg[1];
+                    bAvg[2] = (byte)iAvg[2];
+                    bAvg[3] = (byte)iAvg[3];
+                    SetColorAt(hBlur, i, j, width, bAvg, 4); 
+
+                }
+                
+            }
+            var total = new byte[hBlur.Length];
+            Array.Copy(hBlur, total, hBlur.Length);
+            for (int i = 0; i < width; i++)
+            {
+                int[] tSum = new int[] { 0, 0, 0, 0 };
+                int[] iAvg = new int[] { 0, 0, 0, 0 };
+                for (int y = 0; y < kSize; y++)
+                {
+                    var tmpColor = GetColorAt(hBlur, i, y, 4, width);
+                    tSum[0] += tmpColor[0];
+                    tSum[1] += tmpColor[1];
+                    tSum[2] += tmpColor[2];
+                    tSum[3] += tmpColor[3];
+                }
+                iAvg[0] = tSum[0] / kSize;
+                iAvg[1] = tSum[1] / kSize;
+                iAvg[2] = tSum[2] / kSize;
+                iAvg[3] = tSum[3] / kSize;
+
+                for (int j = 0; j < height; j++)
+                {
+                    if(j - kSize/2 >= 0 && j + 1 + kSize/2 < height)
+                    {
+                        var tmp_pColor = GetColorAt(hBlur, i, j - kSize / 2, 4, width);
+                        tSum[0] -= tmp_pColor[0];
+                        tSum[1] -= tmp_pColor[1]; 
+                        tSum[2] -= tmp_pColor[2];
+                        tSum[3] -= tmp_pColor[3];
+                        var tmp_nColor = GetColorAt(hBlur, i, j + 1 + kSize / 2, 4 , width);
+                        tSum[0] += tmp_nColor[0];
+                        tSum[1] += tmp_nColor[1];
+                        tSum[2] += tmp_nColor[2];
+                        tSum[3] += tmp_nColor[3];
+
+                        iAvg[0] = tSum[0] / kSize;
+                        iAvg[1] = tSum[1] / kSize;
+                        iAvg[2] = tSum[2] / kSize;
+                        iAvg[3] = tSum[3] / kSize;
+
+                    }
+                    var bAvg = new byte[iAvg.Length];
+                    bAvg[0] = (byte)iAvg[0];
+                    bAvg[1] = (byte)iAvg[1];
+                    bAvg[2] = (byte)iAvg[2];
+                    bAvg[3] = (byte)iAvg[3];
+                    SetColorAt(hBlur, i, j, width, bAvg, 4);
+                }
+            }
+            return total;
+
+        }
+    }
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/FastBlurTest.cs
@@ -0,0 +1,57 @@
+﻿using com.csutil.algorithms.images;
+using com.csutil.io;
+using com.csutil.model;
+using StbImageWriteSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Zio;
+
+namespace com.csutil.tests.AlgorithmTests
+{
+    public class FastBlurTest
+    {
+        public FastBlurTest(Xunit.Abstractions.ITestOutputHelper logger) { logger.UseAsLoggingOutput(); }
+
+        [Fact]
+        public async Task RunBoxBlur_ShouldApplyBlurCorrectly()
+        {
+
+            var folder = EnvironmentV2.instance.GetOrAddAppDataFolder("FastBlurTesting");
+
+            var imageFile = folder.GetChild("GT04-image.png");
+            await DownloadFileIfNeeded(imageFile, "http://atilimcetin.com/global-matting/GT04-image.png");
+
+            var image = await ImageLoader.LoadImageInBackground(imageFile);
+            var imageResult = FastBlur.FastBoxBlur(image.Data, image.Width, image.Height, 21, (int)image.ColorComponents);
+            var flippedResult = ImageUtility.FlipImageVertically(imageResult, image.Width, image.Height, (int)image.ColorComponents);
+            var test = folder.GetChild("FastBlurred.png");
+            {
+                using var stream = test.OpenOrCreateForWrite();
+                ImageWriter writer = new ImageWriter();
+                writer.WritePng(flippedResult, image.Width, image.Height, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
+            }
+        }
+        private static async Task DownloadFileIfNeeded(FileEntry self, string url)
+        {
+            var imgFileRef = new MyFileRef() { url = url, fileName = self.Name };
+            await imgFileRef.DownloadTo(self.Parent, useAutoCachedFileRef: true);
+        }
+        private class MyFileRef : IFileRef
+        {
+            public string dir { get; set; }
+            public string fileName { get; set; }
+            public string url { get; set; }
+            public Dictionary<string, object> checksums { get; set; }
+            public string mimeType { get; set; }
+        }
+    }
+}
+
+
+
+
+
