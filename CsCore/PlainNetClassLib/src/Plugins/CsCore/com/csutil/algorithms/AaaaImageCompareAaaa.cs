cf828e6791064f5f63fa990fef78521b5c91ecfb
XANGEnino <nino@menzel-online.net>
2024-01-23 12:55:22 +0100

    Created our method file location, with function ImageCompare and created test for the method

---
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/AaaaImageCompareAaaa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StbImageSharp.Utility;
+using StbImageSharp.Decoding;
+using com.csutil.io;
+
+namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms
+{
+    public class AaaaImageCompareAaaa
+    {
+
+        public AaaaImageCompareAaaa()
+        {
+            
+
+
+        }
+
+        public int ImageCompare()
+        {
+
+            return 100;
+
+        }
+
+    }
+}
---
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageCompareTests.cs
@@ -0,0 +1,26 @@
+﻿using com.csutil.src.Plugins.CsCore.com.csutil.algorithms;
+using Xunit;
+
+namespace xUnitTests.src.Plugins.CsCoreXUnitTests.com.csutil.tests.AlgorithmTests
+{
+    public class ImageCompareTests
+    {
+
+        public ImageCompareTests(Xunit.Abstractions.ITestOutputHelper logger) { 
+        
+            logger.UseAsLoggingOutput();
+
+        }
+
+        [Fact]
+        public void TestImageCompare()
+        {
+
+            AaaaImageCompareAaaa compareObj = new AaaaImageCompareAaaa();
+            Assert.Equal(100, compareObj.ImageCompare());
+
+        }
+
+    }
+
+}
