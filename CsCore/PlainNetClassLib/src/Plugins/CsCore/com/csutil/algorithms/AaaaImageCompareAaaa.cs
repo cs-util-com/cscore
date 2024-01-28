aa0b146be5ca5f654b100a233c23e4a912a8e646
nino_mzl <nino@menzel-online.net>
2024-01-28 13:03:32 +0100

    Added simple ImageCompare method, which compares the color of each pixel with a ColorLoad function of an ImageResult class

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/AaaaImageCompareAaaa.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/AaaaImageCompareAaaa.cs
@@ -2,25 +2,56 @@
 using StbImageSharp.Utility;
 using StbImageSharp.Decoding;
 using com.csutil.io;
+using StbImageSharp;
+using System.Drawing;
+using System.Linq;
 
 namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms
 {
-    public class AaaaImageCompareAaaa
-    {
+    public class AaaaImageCompareAaaa {
 
-        public AaaaImageCompareAaaa()
-        {
-            
+        public AaaaImageCompareAaaa() {
+
 
 
         }
 
-        public int ImageCompare()
-        {
+        public bool ImageCompare(ImageResult img1, ImageResult img2) {
 
-            return 100;
+            Color[] colors_img1 = LoadImageColors(img1);
+            Color[] colors_img2 = LoadImageColors(img2);
+
+            // Compare Images
+            for (int i = 0; i < img1.Width; i++) {
+
+                if (colors_img1[i] != colors_img2[i]) {
+                    return false;
+                }
+
+            }
+
+            return true;
+
+        }
+
+        private Color[] LoadImageColors(ImageResult img) {
+
+            Color[] colors = new Color[img.Data.Length / 4];
+            for (int i = 0; i < colors.Length; i++) {
+
+                colors[i] = Color.FromArgb(
+                        img.Data[i * 4 + 3],
+                        img.Data[i * 4],
+                        img.Data[i * 4 + 1],
+                        img.Data[i * 4 + 2]
+                    );
+
+            }
+
+            return colors;
 
         }
 
     }
+
 }
--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageCompareTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageCompareTests.cs
@@ -1,5 +1,8 @@
-﻿using com.csutil.src.Plugins.CsCore.com.csutil.algorithms;
+﻿using com.csutil.io;
+using com.csutil.model;
+using System.Collections.Generic;
 using Xunit;
+using Zio;
 
 namespace com.csutil.tests.AlgorithmTests {
     public class ImageCompareTests {
@@ -11,12 +14,34 @@ namespace com.csutil.tests.AlgorithmTests {
         }
 
         [Fact]
-        public void TestImageCompare()
+        public async void TestImageCompare()
         {
 
-            AaaaImageCompareAaaa compareObj = new AaaaImageCompareAaaa();
-            Assert.Equal(100, compareObj.ImageCompare());
+            var dir = EnvironmentV2.instance.GetOrAddTempFolder("TestImageComparePictures");
 
+            var imgRef = new FileRef() { url = "https://placekitten.com/1024/512" };
+            await imgRef.DownloadTo(dir);
+            Log.d("FileRef: " + JsonWriter.AsPrettyString(imgRef));
+            Assert.NotNull(imgRef.url);
+            Assert.NotNull(imgRef.fileName);
+            Assert.NotNull(imgRef.dir);
+
+            FileEntry imgEntry = imgRef.GetFileEntry(dir.FileSystem);
+            var img = await ImageLoader.LoadImageInBackground(imgEntry);
+            Assert.Equal(1024, img.Width);
+            Assert.Equal(512, img.Height);
+
+            byte[] byteArray = new byte[] { 1, 2, 1 };
+            Assert.Equal(byteArray, img.Data);
+
+        }
+
+        private class FileRef : IFileRef {
+            public string dir { get; set; }
+            public string fileName { get; set; }
+            public string url { get; set; }
+            public Dictionary<string, object> checksums { get; set; }
+            public string mimeType { get; set; }
         }
 
     }
