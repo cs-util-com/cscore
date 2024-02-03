c6716af779e480c102ee8532bfb8f1f1d8344d7a
nino_mzl <nino@menzel-online.net>
2024-02-03 14:03:59 +0100

    Added MeanSquare Error measure for distortion in ImageCompare method. Created CompareResult class for structured return in ImageCompare. Changed the test to work with new CompareResult class. The resulting picture now changes the red value of a picture depending on the distortion. There is a value on how strongly the output red will show the differences (maybe put it as input in the ImageCompare method for flexible use.

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/AaaaImageCompareAaaa.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/algorithms/AaaaImageCompareAaaa.cs
@@ -1,5 +1,7 @@
 ﻿using StbImageSharp;
+using System;
 using System.Drawing;
+using System.Numerics;
 
 namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms {
 
@@ -7,7 +9,9 @@ namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms {
 
         public AaaaImageCompareAaaa() { }
 
-        public ImageResult ImageCompare(ImageResult img1, ImageResult img2) {
+        public CompareResult ImageCompare(ImageResult img1, ImageResult img2) {
+
+            CompareResult result = new CompareResult();
 
             ImageResult resultImage = new ImageResult();
             resultImage.Data = img1.Data.DeepCopy();
@@ -16,24 +20,43 @@ namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms {
 
             // Check if images are the same size
             if (img1.Width != img2.Width && img1.Height != img2.Height) {
-                return resultImage;
+                result.resultImage = resultImage;
+                return result;
             }
 
             Color[] colors_img1 = LoadImageColors(img1);
             Color[] colors_img2 = LoadImageColors(img2);
+            double squaredMeanSum = 0.0d;
 
             // Compare images
             for (int i = 0; i < colors_img1.Length; i++) {
 
+                // Get pixel distortion
+                Vector3 diff = new Vector3(
+                    colors_img1[i].R - colors_img2[i].R,
+                    colors_img1[i].G - colors_img2[i].G,
+                    colors_img1[i].B - colors_img2[i].B
+                );
+
+                squaredMeanSum += diff.LengthSquared();
+
                 if (colors_img1[i] != colors_img2[i]) {
 
-                    ParseColorIntoData(resultImage, Color.FromArgb(255, 255, 0, 0), i);
+                    double ratio = diff.LengthSquared() / 195075.0d;
+
+                    int red = (int)Math.Max(0, Math.Min(255, colors_img1[i].R * 15 * ratio + colors_img1[i].R));
+                    ParseColorIntoData(resultImage, Color.FromArgb(255, red, colors_img1[i].G, colors_img1[i].B), i);
 
                 }
 
             }
 
-            return resultImage;
+            double squaredMean = squaredMeanSum / (img1.Width * img1.Height);
+
+            result.resultImage = resultImage;
+            result.distortion = squaredMean;
+
+            return result;
 
         }
 
@@ -61,6 +84,13 @@ namespace com.csutil.src.Plugins.CsCore.com.csutil.algorithms {
             img.Data[index * 4 + 1] = color.G;
             img.Data[index * 4 + 2] = color.B;
             img.Data[index * 4 + 3] = color.A;
+
+        }
+
+        public class CompareResult {
+
+            public ImageResult resultImage;
+            public double distortion;
 
         }
 
--- CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageCompareTests.cs
+++ CsCore/xUnitTests/src/Plugins/CsCoreXUnitTests/com/csutil/tests/AlgorithmTests/ImageCompareTests.cs
@@ -25,9 +25,10 @@ namespace com.csutil.tests.AlgorithmTests {
             var dir = EnvironmentV2.instance.GetOrAddTempFolder(tmpFolderName);
 
             FileRef[] imgRef = {
-                //new FileRef { url = "https://wgrt.com/wp-content/uploads/Cat-Problem-1024x512.png" },
-                new FileRef { url = "https://www.veterinaire.mu/wp-content/uploads/2021/05/cat-1024x512.png" },
-                new FileRef { url = "https://i.imgur.com/0FEVxnA.png" }
+                new FileRef { url = "https://wgrt.com/wp-content/uploads/Cat-Problem-1024x512.png" }, // cat standing up
+                new FileRef { url = "https://www.veterinaire.mu/wp-content/uploads/2021/05/cat-1024x512.png" }, // base without line
+                //new FileRef { url = "https://i.imgur.com/0FEVxnA.png" } // blue line
+                //new FileRef { url = "https://i.imgur.com/6w9dzEk.png" } // brown line
             };
 
             ImageResult[] images = new ImageResult[3];
@@ -46,38 +47,39 @@ namespace com.csutil.tests.AlgorithmTests {
                 Assert.Equal(1024, images[i].Width);
                 Assert.Equal(512, images[i].Height);
 
-
             }
 
             // Compare test for different images -> should result in differences
             AaaaImageCompareAaaa compareObj = new AaaaImageCompareAaaa();
-            ImageResult result = compareObj.ImageCompare(images[0], images[1]);
+            AaaaImageCompareAaaa.CompareResult result = compareObj.ImageCompare(images[0], images[1]);
 
-            Assert.Equal(1024, result.Width);
-            Assert.Equal(512, result.Height);
+            Assert.Equal(1024, result.resultImage.Width);
+            Assert.Equal(512, result.resultImage.Height);
 
-            result.Data = FlipImageHorizontally(result.Data, result.Width, result.Height, result.Data.Length);
+            result.resultImage.Data = FlipImageHorizontally(result.resultImage.Data, result.resultImage.Width, result.resultImage.Height, result.resultImage.Data.Length);
+            Log.d("Distortion: " + result.distortion);
 
             using (Stream stream = File.OpenWrite(Path.Combine(Path.GetTempPath(), tmpFolderName + "\\result.png"))) {
 
                 ImageWriter writer = new ImageWriter();
-                writer.WritePng(result.Data, 1024, 512, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
+                writer.WritePng(result.resultImage.Data, 1024, 512, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
                 Log.d("Image Out.");
 
             }
 
             // Compare test for the same image -> should result in no differences
-            ImageResult result2 = compareObj.ImageCompare(images[0], images[0]);
+            AaaaImageCompareAaaa.CompareResult result2 = compareObj.ImageCompare(images[0], images[0]);
 
-            Assert.Equal(1024, result2.Width);
-            Assert.Equal(512, result2.Height);
+            Assert.Equal(1024, result2.resultImage.Width);
+            Assert.Equal(512, result2.resultImage.Height);
 
-            result2.Data = FlipImageHorizontally(result2.Data, result2.Width, result2.Height, result2.Data.Length);
+            result2.resultImage.Data = FlipImageHorizontally(result2.resultImage.Data, result2.resultImage.Width, result2.resultImage.Height, result2.resultImage.Data.Length);
+            Log.d("Distortion: " + result2.distortion);
 
             using (Stream stream = File.OpenWrite(Path.Combine(Path.GetTempPath(), tmpFolderName + "\\result2.png"))) {
 
                 ImageWriter writer = new ImageWriter();
-                writer.WritePng(result2.Data, 1024, 512, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
+                writer.WritePng(result2.resultImage.Data, 1024, 512, StbImageWriteSharp.ColorComponents.RedGreenBlueAlpha, stream);
                 Log.d("Image Out.");
 
             }
