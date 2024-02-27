cab00c180907d4de850e26bb045c45f4ce728153
S <code@csutil.com>
2024-02-27 09:58:21 +0100

    Adding more xunit mock classes for Unity compatibility

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/testing/xunitmocks/EqualException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+using JetBrains.Annotations;
+
+namespace Xunit.Sdk {
+
+    public class EqualException : Exception {
+
+        public EqualException() { }
+        protected EqualException([NotNull] SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public EqualException(string message) : base(message) { }
+        public EqualException(string message, Exception innerException) : base(message, innerException) { }
+
+        public EqualException(object expected, object actual) : this($"Expected: {expected} Actual: {actual}") { }
+
+    }
+
+}
