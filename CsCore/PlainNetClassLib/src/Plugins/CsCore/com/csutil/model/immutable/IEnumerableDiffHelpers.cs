1fbd2fe03873d21bdc09437ca13f8892bce3bbb2
S <code@csutil.com>
2024-12-10 07:23:20 +0100

    Introduced a new slightly faster CalcEntryChangesToOldStateV3 for dictionaries that produces less memory garbage

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/IEnumerableDiffHelpers.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/IEnumerableDiffHelpers.cs
@@ -7,10 +7,12 @@ namespace com.csutil {
 
     public static class IEnumerableDiffHelpers {
 
+        [Obsolete("Use CalcEntryChangesToOldStateV3 instead", true)]
         public static void CalcEntryChangesToOldStateV2<E, K, V>(this E newState, ref E oldState, Action<KeyValuePair<K, V>> onEntryAdded, Action<KeyValuePair<K, V>, KeyValuePair<K, V>> onEntryUpdated, Action<K> onEntryRemoved) where E : IReadOnlyDictionary<K, V> {
             CalcEntryChangesToOldState(newState, ref oldState, getKey: x => x.Key, onEntryAdded, onEntryUpdated, onEntryRemoved);
         }
 
+        [Obsolete("Use CalcEntryChangesToOldStateV3 instead", true)]
         public static void CalcEntryChangesToOldState<E, K, V>(this E newState, ref E oldState, Action<KeyValuePair<K, V>> onEntryAdded, Action<KeyValuePair<K, V>, KeyValuePair<K, V>> onEntryUpdated, Action<K> onEntryRemoved) where E : IDictionary<K, V> {
             CalcEntryChangesToOldState(newState, ref oldState, getKey: x => x.Key, onEntryAdded, onEntryUpdated, onEntryRemoved);
         }
@@ -53,6 +55,43 @@ namespace com.csutil {
                 onEntryAdded(newLeft.Value);
             }
 
+        }
+
+        public static void CalcEntryChangesToOldStateV3<E, K, V>(this E newState, ref E oldState, Action<K, V> onEntryAdded, Action<K, V, V> onEntryUpdated, Action<K> onEntryRemoved) where E : IDictionary<K, V> {
+
+            var oldStateCopy = oldState;
+            oldState = newState;
+            if (ReferenceEquals(oldStateCopy, newState)) {
+                Log.w("CalcEntryChangesToOldStateV3: oldStateCopy == newState");
+                return;
+            }
+            if (oldStateCopy == null) {
+                foreach (var kv in newState) { onEntryAdded(kv.Key, kv.Value); }
+                return;
+            }
+            if (newState == null) {
+                foreach (var oldKv in oldStateCopy) { onEntryRemoved(oldKv.Key); }
+                return;
+            }
+
+            // Check for removed or updated entries:
+            foreach (var oldKv in oldStateCopy) {
+                K key = oldKv.Key;
+                V oldVal = oldKv.Value;
+                if (!newState.TryGetValue(key, out V newVal)) {
+                    onEntryRemoved(key);
+                } else {
+                    if (StateCompare.WasModified(oldVal, newVal)) { onEntryUpdated(key, oldVal, newVal); }
+                }
+            }
+
+            // Check for newly added entries:
+            foreach (var newKv in newState) {
+                K key = newKv.Key;
+                if (oldStateCopy == null || !oldStateCopy.ContainsKey(key)) {
+                    onEntryAdded(key, newKv.Value);
+                }
+            }
         }
 
     }
--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/ImmutableCollectionExtensions.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/model/immutable/ImmutableCollectionExtensions.cs
@@ -6,12 +6,23 @@ namespace com.csutil.model.immutable {
 
     public static class ImmutableCollectionExtensions {
 
+        [Obsolete("Use AddStateChangeListenerForDictionaryV2 instead", true)]
         public static Action AddStateChangeListenerForDictionary<T, K, V>(this IDataStore<T> self, Func<T, ImmutableDictionary<K, V>> getSubState, Action<KeyValuePair<K, V>> onEntryAdded,
             Action<KeyValuePair<K, V>, KeyValuePair<K, V>> onEntryUpdated, Action<K> onEntryRemoved) {
             {
                 var oldDictionaryState = getSubState(self.GetState());
                 return self.AddStateChangeListener(getSubState, (newDictionary) => {
                     newDictionary.CalcEntryChangesToOldState(ref oldDictionaryState, onEntryAdded, onEntryUpdated, onEntryRemoved);
+                }, triggerInstantToInit: false);
+            }
+        }
+        
+        public static Action AddStateChangeListenerForDictionaryV2<T, K, V>(this IDataStore<T> self, Func<T, ImmutableDictionary<K, V>> getSubState, Action<K, V> onEntryAdded,
+            Action<K, V, V> onEntryUpdated, Action<K> onEntryRemoved) {
+            {
+                var oldDictionaryState = getSubState(self.GetState());
+                return self.AddStateChangeListener(getSubState, (newDictionary) => {
+                    newDictionary.CalcEntryChangesToOldStateV3(ref oldDictionaryState, onEntryAdded, onEntryUpdated, onEntryRemoved);
                 }, triggerInstantToInit: false);
             }
         }
