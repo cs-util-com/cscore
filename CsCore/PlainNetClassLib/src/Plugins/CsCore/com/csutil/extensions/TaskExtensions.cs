beb996d8f596734928398566dff9bd3b3d88ba9e
S <code@csutil.com>
2025-01-01 12:13:53 +0100

    Improvements to LogOnError

--- CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/extensions/TaskExtensions.cs
+++ CsCore/PlainNetClassLib/src/Plugins/CsCore/com/csutil/extensions/TaskExtensions.cs
@@ -32,24 +32,30 @@ namespace com.csutil {
             await self; // use await to propagate exceptions
         }
 
-        public static Task OnError(this Task self, Func<Exception, Task> onError) {
-            try { // Catch in case current sync context is not allowed to be used as a scheduler (e.g in xUnit)
-                return self.ContinueWith(HandleIfError(onError), TaskScheduler.FromCurrentSynchronizationContext()).Unwrap();
-            } catch (Exception) { return self.ContinueWith(HandleIfError(onError)).Unwrap(); }
+        public static async Task OnError(this Task self, Func<Exception, Task> onError) {
+            try {
+                await self;
+            } catch (Exception ex) {
+                await onError(ex).ConfigureAwait(false);
+            }
         }
 
-        public static Task<T> OnError<T>(this Task<T> self, Func<Exception, Task<T>> onError) {
-            try { // Catch in case current sync context is not allowed to be used as a scheduler (e.g in xUnit)
-                return self.ContinueWith(HandleIfError(onError), TaskScheduler.FromCurrentSynchronizationContext()).Unwrap();
-            } catch (Exception) { return self.ContinueWith(HandleIfError(onError)).Unwrap(); }
+        public static async Task<T> OnError<T>(this Task<T> self, Func<Exception, Task<T>> onError) {
+            try {
+                return await self;
+            } catch (Exception ex) {
+                // Handle the error and return fallback value:
+                return await onError(ex).ConfigureAwait(false);
+            }
         }
 
-        private static Func<T, T> HandleIfError<T>(Func<Exception, T> onError) where T : Task {
-            return task => { if (task.IsFaulted) { return onError(task.Exception); } else { return task; } };
-        }
-
-        public static Task LogOnError(this Task self) {
-            return self.OnError(e => Task.FromException(Log.e(e)));
+        public static async Task LogOnError(this Task self) {
+            try {
+                await self;
+            } catch (Exception e) {
+                Log.e(e);
+                throw;
+            }
         }
 
         /// <summary> Ensures that the continuation action is called on the same syncr. context </summary>
