abb3a7c05413e408fd4b6f563fa5c5ecf6fa2281
S <code@csutil.com>
2025-07-04 06:31:11 +0200

    Added mock Trait class for xUnit test compatibility in Unity

---
+++ CsCore/CsCoreUnity/Plugins/CsCoreUnity/com/csutil/testing/xunitmocks/Trait.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xunit {
+
+    /// <summary>
+    /// Mock for the Xunit Trait attribute, which is used to categorize tests.
+    /// Annotation looks like this:
+    /// [Trait("Category","SkipInBuildPipeline")]
+    /// </summary>
+    public class Trait : Attribute {
+
+        public string Category { get; }
+        public string Value { get; }
+
+        public Trait(string category, string value) {
+            Category = category;
+            Value = value;
+        }
+
+    }
+
+}
